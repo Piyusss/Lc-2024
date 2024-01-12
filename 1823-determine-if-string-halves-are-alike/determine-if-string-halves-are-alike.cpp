97e054acdf0fa1e4dbbc2cae2a2e1ad0e85a3bcb
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-01-12 22:04:52 +0530

    Time: 3 ms (52.79%) | Memory: 7.2 MB (24.18%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool halvesAreAlike(string s) {
         int n=s.length();
-        int cnt1=0;int cnt2=0;
+        int cnt1=0;
         set<char>st={'a','e','i','o','u','A','E','I','O','U'};
         for(int i=0;i<n;i++){
             if(st.find(s[i])!=st.end()){
                 if(i<n/2)cnt1++;
-                else cnt2++;
+                else cnt1--;
             }
         }
         //now
-        if(cnt1==cnt2)return 1;
+        if(cnt1==0)return 1;
         return 0;
 
     }
