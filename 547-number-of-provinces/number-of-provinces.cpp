a6721c687410cb579734a37494128f25d48641ae
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-01-12 22:52:29 +0530

    Time: 21 ms (72.73%) | Memory: 15.8 MB (70.45%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,31 +1,31 @@
 class DisjointSet{
+    vector<int>rank,parent;
     public:
-    vector<int>rank,parent,size;
     DisjointSet(int n){
         rank.resize(n+1,0);
         parent.resize(n+1);
-        size.resize(n+1);
         for(int i=0 ; i<=n ; i++){
             parent[i]=i;
-            size[i]=1;
         }
     }
     int findUPar(int node){
         if(node==parent[node])return node;
         return parent[node]=findUPar(parent[node]);
     }
-    void unionBySize(int u,int v){
+    void unionByRank(int u,int v){
         int ulp_u=findUPar(u);
         int ulp_v=findUPar(v);
         if(ulp_u==ulp_v)return;
-        if(size[ulp_u]<size[ulp_v]){
+            if(rank[ulp_u]<rank[ulp_v]){
             parent[ulp_u]=ulp_v;
-            size[ulp_v]+=size[ulp_u];
-        }
-        else {
-            parent[ulp_v]=ulp_u;
-            size[ulp_u]+=size[ulp_v];
-        }
+            }
+            else if(rank[ulp_v]<rank[ulp_u]){
+                parent[ulp_v]=ulp_u;
+            }
+            else {
+                parent[ulp_v]=ulp_u;
+                rank[ulp_u]++;
+            }
     }
 };
 
@@ -41,14 +41,14 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(adjMat[i][j]==1){
-                    ds.unionBySize(i+1,j+1);
+                    ds.unionByRank(i+1,j+1);
                 }
             }
         }
         //now
         int cnt=0;
         for(int i=0;i<n;i++){
-            if(ds.parent[i+1]==i+1)cnt++;
+            if(ds.findUPar(i+1)==i+1)cnt++;
         }
         return cnt;
     }
