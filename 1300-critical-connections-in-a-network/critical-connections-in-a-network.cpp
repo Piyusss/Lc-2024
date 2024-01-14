dcf68f1c368df9f0a878e54ae48ed1584705bf4b
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-01-14 11:35:45 +0530

    Time: 462 ms (69.68%) | Memory: 173.8 MB (59.29%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -1,23 +1,17 @@
 class Solution {
-    private:
     int timer=1;
-    void dfs(int node,int par,vector<int>&vis,vector<int>adj[],int tin[],int low[],vector<vector<int>>&bridges){
-        vis[node]=1;
-        tin[node]=low[node]=timer;
-        timer++;
+    void dfs(int node,int par,vector<int>&vis,vector<int>adj[],
+    int tin[],int low[],vector<vector<int>>&bridges){
+        vis[node]=1;tin[node]=low[node]=timer;timer++;
         for(auto it:adj[node]){
-            if(it==par)continue;
-            if(!vis[it]){
+            if(it!=par){
+                if(!vis[it]){
                 dfs(it,node,vis,adj,tin,low,bridges);
                 low[node]=min(low[node],low[it]);
-                //now
-                if(low[it]>tin[node]){
-                    bridges.push_back({node,it});
-                }
+                if(low[it]>tin[node])bridges.push_back({node,it});
             }
-            else{
-                low[node]=min(low[node],low[it]);
-            }
+            else low[node]=min(low[node],low[it]);
+            }          
         }
     }
 public:
@@ -25,15 +19,10 @@ public:
         //tarzans-algorithm
         vector<int>adj[n];
         for(auto it:connections){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+            adj[it[0]].push_back(it[1]);adj[it[1]].push_back(it[0]);
         }
-        vector<int>vis(n,0);
-        int tin[n];
-        int low[n];
-
         vector<vector<int>>bridges;
-        //now
+        vector<int>vis(n,0);int tin[n];int low[n];
         dfs(0,-1,vis,adj,tin,low,bridges);
         return bridges;
     }
