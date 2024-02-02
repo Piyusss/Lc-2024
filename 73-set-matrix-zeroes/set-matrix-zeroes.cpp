1aacae2aa0be3559cf45ef709f8cd30a62ad99ca
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-02-02 18:46:42 +0530

    Time: 11 ms (60.51%) | Memory: 16.5 MB (6.45%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,23 +3,22 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> visited = matrix;
+        int row[200] = {0}; // row array
+        int col[200] = {0}; // col array
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(matrix[i][j] == 0){
-                    for(int k=0; k<m; k++){
-                        visited[i][k] = 0;
-                    }
-                    for(int k=0; k<n; k++){
-                        visited[k][j] = 0;
-                    }
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
         //now
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                matrix[i][j] = visited[i][j];
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
+                }
             }
         }
     }
