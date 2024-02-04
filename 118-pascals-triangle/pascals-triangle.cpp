8f235a62b19f11536e50ae5085cb5a5af805ad64
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-02-04 11:34:18 +0530

    Time: 4 ms (29.71%) | Memory: 8 MB (17.15%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-// unsigned long long fact(int n){
-//     if(n==0 || n==1)return 1;
-//     return n*fact(n-1);
-// }
-unsigned long long f(int n,int r){
-    unsigned long long res=1;
-    for(int i=0;i<r;i++){
-        res*=(n-i);
-        res/=(i+1);
+vector<int>generateRow(int row){
+    long long ans=1;
+    vector<int>ansRow;
+    ansRow.push_back(1);
+    for(int col=1;col<=row-1;col++){
+        ans=ans*(row-col);
+        ans=ans/(col);
+        ansRow.push_back(ans);
     }
-    return res;
+    return ansRow;
 }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>ans;
-        for(int i=0;i<numRows;i++){
-            vector<int>temp;
-            for(int j=0;j<i+1;j++){
-                temp.push_back(f(i,j));
-            }
-            ans.push_back(temp);
+        for(int i=1;i<=numRows;i++){
+            ans.push_back(generateRow(i));
         }
         return ans;
     }
