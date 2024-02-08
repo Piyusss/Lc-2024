317a5a73ad332d6799e2f9ffa36d68728bee08ec
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-02-08 22:09:02 +0530

    Time: 386 ms (25.41%) | Memory: 513.9 MB (5.00%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-         int n = s.length();
-    int maxLength = 1;
-    int start = 0;
-
-    for (int i = 0; i < n; i++) {
-        // Check for odd-length palindromes
-        int left = i;
-        int right = i;
-        while (left >= 0 && right < n && s[left] == s[right]) {
-            int currentLength = right - left + 1;
-            if (currentLength > maxLength) {
-                maxLength = currentLength;
-                start = left;
-            }
-            left--;
-            right++;
-        }
-
-        // Check for even-length palindromes
-        left = i;
-        right = i + 1;
-        while (left >= 0 && right < n && s[left] == s[right]) {
-            int currentLength = right - left + 1;
-            if (currentLength > maxLength) {
-                maxLength = currentLength;
-                start = left;
-            }
-            left--;
-            right++;
-        }
+bool isPal(string s,int i,int j){
+    while(i<=j){
+        if(s[i]!=s[j])return 0;
+        i++;j--;
     }
-
-    return s.substr(start, maxLength);
+    return 1;
+}
+    string longestPalindrome(string s) {
+        int n=s.size();int start=0;int max_len=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(s[i]==s[j] && (j-i+1)>max_len && isPal(s,i,j)){
+                    max_len=j-i+1;
+                    start=i;
+                }
+            }
+        }
+        return s.substr(start,max_len);
     }
 };
