a6c143451f8c71bd873f29fe44c5fbf7cd18dc39
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-02-10 11:50:59 +0530

    Time: 2 ms (47.58%) | Memory: 7 MB (29.82%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        int a[40];
-        a[0]=0;
-        a[1]=1;
-        a[2]=1;
-        for(int i=3;i<=n;i++){
-            a[i]=a[i-1]+a[i-2]+a[i-3];
+        if(n==0)return 0;
+        if(n==1)return 1;
+        if(n==2)return 1;
+        int sslast=0;
+        int secondLast = 1;
+        int last = 1;
+        int curr = 2;
+        
+        n = n-2;
+        while(n>0){
+            curr = (last + secondLast + sslast);
+            sslast = secondLast;
+            secondLast = last;
+            last=curr;
+            n--;
         }
-        return a[n];
+        return curr;
     }
 };
