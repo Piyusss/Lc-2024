32a281d30c75fbf120a1dd61794f5201dca4f2a4
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-02-11 11:53:52 +0530

    Time: 66 ms (68.05%) | Memory: 11.6 MB (83.43%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -3,13 +3,14 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,0)));
+        vector<vector<int>>front(m+1,vector<int>(m+1,0));
+        vector<vector<int>>cur(m+1,vector<int>(m+1,0));
         
         //base-tabulation
         for(int j1=0;j1<m;j1++){
             for(int j2=0;j2<m;j2++){
-                if(j1==j2)dp[n-1][j1][j2]=grid[n-1][j1];
-                else dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+                if(j1==j2)front[j1][j2]=grid[n-1][j1];
+                else front[j1][j2]=grid[n-1][j1]+grid[n-1][j2];
             }
         }
 
@@ -23,15 +24,16 @@ public:
                             if(j1==j2)val+=grid[i][j1];
                             else val+=grid[i][j1]+grid[i][j2];
                             if(j1+dj1>=0 && j1+dj1<m && j2+dj2>=0 && j2+dj2<m)
-                                val+=dp[i+1][j1+dj1][j2+dj2];
+                                val+=front[j1+dj1][j2+dj2];
                             else 
                                 val+=-1e8;
                             maxi=max(maxi,val);
                         }
                     }
-                    dp[i][j1][j2]=maxi;
+                    cur[j1][j2]=maxi;
             }
+            front=cur;
         }
-        return dp[0][0][m-1];
+        return front[0][m-1];
     }
 };
