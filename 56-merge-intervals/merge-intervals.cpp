8523c10888aa05d59c2cb58c1ce95e83e0c1c169
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-02-14 00:31:58 +0530

    Time: 24 ms (77.24%) | Memory: 22.6 MB (39.07%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,15 +5,8 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         for(int i=0;i<n;i++){
-            int start=nums[i][0];
-            int end=nums[i][1];
-            if(!ans.empty() && end<=ans.back()[1])continue;
-
-            for(int j=i+1;j<n;j++){
-                if(nums[j][0]<=end)end=max(end,nums[j][1]);
-                else break;
-            }
-            ans.push_back({start,end});
+            if(ans.empty() || nums[i][0]>ans.back()[1])ans.push_back(nums[i]);
+            else ans.back()[1]=max(ans.back()[1],nums[i][1]);
         }
         return ans;
     }
