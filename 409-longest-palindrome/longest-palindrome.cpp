6fca7d5c5633f4f945bc385cc6edd0150b5bd1e5
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-02-13 20:10:33 +0530

    Time: 3 ms (56.54%) | Memory: 8.9 MB (21.34%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -6,45 +6,21 @@ public:
          for(auto it:s)mp[it]++;
          int cnt=0;int flag=0;
          //now
-         if(n%2==0){
-             for(auto it:mp){
-                 if(it.second%2==0){
-                     cnt+=it.second;
-                 }
-             }
-             for(auto it:mp){
-                 if(it.second%2!=0){
-                     cnt+=((it.second)/2)*(2);
-                     flag=1;
-                 }
-             }
-             for(auto it:mp){
-                 if(it.second==1 || flag){
-                     cnt++;
-                     break;
-                 }
-             }
-        }
-        //now
-        else{
+         for(auto it:mp){
+                if(it.second%2==0)cnt+=it.second;
+            }
             for(auto it:mp){
-                 if(it.second%2==0){
-                     cnt+=it.second;
-                 }
-             }
-             for(auto it:mp){
-                 if(it.second%2!=0){
-                     cnt+=((it.second)/2)*(2);
-                     flag=1;
-                 }
-             }
-             for(auto it:mp){
-                 if(it.second==1 || flag){
-                     cnt++;
-                     break;
-                 }
-             }
-        }
+                if(it.second%2!=0){
+                    cnt+=((it.second)/2)*(2);
+                    flag=1;
+                }
+            }
+            for(auto it:mp){
+                if(it.second==1 || flag){
+                    cnt++;
+                    break;
+                }
+            }
         return cnt;
     }
 };
