d06c8cb252dcd7a00d0514fb0270b2571df74aff
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-02-17 18:46:14 +0530

    Time: 104 ms (22.31%) | Memory: 87.9 MB (51.44%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -5,15 +5,14 @@ public:
         vector<int>ans(n);
 
         vector<int>prefixSum(n);
-        prefixSum[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefixSum[i]=prefixSum[i-1]+nums[i];
-        }
+        int tot=accumulate(nums.begin(),nums.end(),0);
+        int prefix=0;
 
         for(int i=0;i<n;i++){
-            int leftSum=prefixSum[i]-nums[i];
-            int rightSum=prefixSum[n-1]-prefixSum[i];
+            int leftSum=prefix;
+            int rightSum=tot-leftSum-nums[i];
             ans[i]=(nums[i]*i)-leftSum+rightSum-(nums[i]*(n-i-1));
+            prefix+=nums[i];
         }
         return ans;
     }
