dab5b2d09281235551aa33caad201df288f8762b
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-03-13 10:00:17 +0530

    Time: 6 ms (17.93%) | Memory: 13.3 MB (5.18%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -4,18 +4,14 @@ public:
         vector<int>v1,v2;
         for(int i=1;i<=n;i++){
             v1.push_back(i);
-        }
-        for(int i=n;i>=1;i--){
-            v2.push_back(i);
+            v2.push_back(n-i+1);
         }
         for(int i=1;i<n;i++){
             v1[i]+=v1[i-1];
             v2[i]+=v2[i-1];
         }
 
-        int cnt=1;
-        int i=0;
-        int j=n-1;
+        int cnt=1,i=0,j=n-1;
         while(i<n){
             if(v1[i]==v2[j])return cnt;
             cnt++;
@@ -27,7 +23,7 @@ public:
 };
 // 9
 // 1 2 3 4 5 6 7 8
-    // 1 3 6 10 15 21 28 36
+// 1 3 6 10 15 21 28 36
 
 // 8 7 6 5 4 3 2 1
-    // 8 15 21 26 30 33 35 36
+// 8 15 21 26 30 33 35 36
