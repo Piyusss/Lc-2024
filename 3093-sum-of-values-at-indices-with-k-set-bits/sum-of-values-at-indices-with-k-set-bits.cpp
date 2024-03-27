96bcdf7356639ca76c0c4adb659c9a6ffd81dbda
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-03-27 19:41:30 +0530

    Time: 11 ms (44.66%) | Memory: 24.9 MB (5.77%) - LeetSync

--- 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.cpp
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.cpp
@@ -3,8 +3,12 @@ public:
     int sumIndicesWithKSetBits(vector<int>& nums, int k) {
         int sum=0;
         int n=nums.size();
+        vector<int>ans(n+1,0);
         for(int i=0;i<n;i++){
-            if(k==__builtin_popcount(i))sum+=nums[i];
+            if(i&1)ans[i]=ans[i/2]+1;
+            else ans[i]=ans[i/2];
+
+            if(ans[i]==k)sum+=nums[i];
         }
         return sum;
     }
