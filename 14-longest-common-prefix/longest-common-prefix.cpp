a57fb002cf93e18455b37ba6285cf98e42dfa081
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-03-30 17:37:50 +0530

    Time: 11 ms (5.95%) | Memory: 18.2 MB (5.82%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,14 +3,8 @@ public:
     string longestCommonPrefix(vector<string>& nums) {
         int n=nums.size();
         //now
-        int mini=INT_MAX;
-        for(int i=0;i<n;i++){
-            int size=nums[i].size();
-            mini=min(mini,size);
-        }
-        //now
         int ans=0;string res="";
-        for(int i=0;i<mini;i++){
+        for(int i=0;i<nums[0].size();i++){
             set<char>temp;
             for(auto it:nums)temp.insert(it[i]);
             if(temp.size()==1)res+=nums[0][i];
