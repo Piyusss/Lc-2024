4f1fdb7d509e4d5001f03cdb615eb3084bb5b4c6
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-04-26 22:02:36 +0530

    Update two-sum.cpp

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int t){
         unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
-            if(mp.find(t-nums[i])!=mp.end()){
+            if(mp.find(t-nums[i])!=mp.end()){ 
                 return {i,mp[t-nums[i]]};
             }
             mp[nums[i]]=i;
