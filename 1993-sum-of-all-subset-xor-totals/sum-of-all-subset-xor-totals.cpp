7813927bd15a874f33a8b1af61b973af92714f93
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-05-20 09:32:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (65.34%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-#define ll long long
+
 int ans=0;
 int n;
 
-void func(int ind,int sum,vector<int>&arr){
+void f(int ind,int sum,vector<int>&arr){
   if(ind==n){
     ans+=sum;
     return;
   }
   //pick-up the element
-  func(ind+1,sum^arr[ind],arr);
+  f(ind+1,sum^arr[ind],arr);
 
   //not pick-up the element
-  func(ind+1,sum,arr);
+  f(ind+1,sum,arr);
 }
     int subsetXORSum(vector<int>& nums) {
         n=nums.size();
-        func(0,0,nums);
+        f(0,0,nums);
         return ans;
     }
 };
