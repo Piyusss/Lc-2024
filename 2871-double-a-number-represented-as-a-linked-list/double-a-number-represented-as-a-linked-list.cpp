ccb637718ad69aeb9788aeb4eab951c9454cd9af
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-05-23 00:20:53 +0530

    Time: 175 ms (49.26%) | Memory: 121.3 MB (30.38%) - LeetSync

---
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode* f(ListNode* head){
+    if(head==NULL || head->next==NULL)return head;
+    ListNode* last=f(head->next);
+    head->next->next=head;
+    head->next=NULL;
+    return last;
+}
+    ListNode* doubleIt(ListNode* head) {
+        head=f(head);
+
+        ListNode* temp=head;
+        ListNode* prev=temp;
+        int car=0;
+        while(temp){
+            int new_val=2*temp->val+car;
+            temp->val=( new_val>=10 ? new_val-10 : new_val );
+            if(new_val>=10)car=1;
+            else car=0;
+            prev=temp;
+            temp=temp->next;
+        }
+
+        if(car==1){
+            prev->next=new ListNode(1);
+        }
+        
+        head=f(head);
+        return head;
+    }
+};
+
+//1 8 9
+//9 8 1->reversed
+//8 7 3->reverse it.
+
+//3 7 8.->ans.
