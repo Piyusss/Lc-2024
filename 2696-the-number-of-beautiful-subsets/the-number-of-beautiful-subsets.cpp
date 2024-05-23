91ae123af968261bcc31b114c7b50f79aa891b60
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-05-23 14:53:49 +0530

    Time: 779 ms (18.18%) | Memory: 38.7 MB (34.71%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
 
 int on;
-int cnt;
+int cnt=0;
 unordered_map<int,int>mp;
 
 void f(int ind,vector<int>&nums,int k){
-    if(ind>=on){
-        cnt++;
-        return;
-    }
+    if(ind==on)return;
 	
 	//not-pick
 	f(ind+1,nums,k);
@@ -18,14 +15,14 @@ void f(int ind,vector<int>&nums,int k){
     if(!mp[nums[ind]+k]  &&  !mp[nums[ind]-k] ){
         mp[nums[ind]]++;
         f(ind+1,nums,k);
+        cnt++;
         mp[nums[ind]]--;
     }
 
 }
     int beautifulSubsets(vector<int>& nums, int k){
-        cnt=0;
         on=nums.size();
         f(0,nums,k);
-        return cnt-1;
+        return cnt;
     }
 };
