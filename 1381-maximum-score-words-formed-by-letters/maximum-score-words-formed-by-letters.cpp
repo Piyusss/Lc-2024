aef9e35226ddaf43e6796f6ac9894042a3155e66
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-05-24 14:57:29 +0530

    Time: 12 ms (51.65%) | Memory: 24.2 MB (23.30%) - LeetSync

---
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+int w;
+int l;
+int s;
+
+map<char,int>mpS;
+int maxScore=INT_MIN;
+
+
+void f(int idx,vector<string>& words,vector<int>& score,int curScore,map<char,int>&mpL){
+    maxScore=max(maxScore,curScore);
+    if(idx>=w)return;
+
+    
+
+    //pick
+    int j=0;
+    int tempScore=0;
+    map<char,int>freqMpL=mpL;
+    while(j<words[idx].length()){
+        if(mpL[words[idx][j]]<=0){
+            mpL=freqMpL;
+            break;
+        }
+        tempScore+=mpS[words[idx][j]];
+        mpL[words[idx][j]]--;
+        j++;
+    }
+    if(j==words[idx].length()){
+        f(idx+1,words,score,curScore+tempScore,mpL);
+    }
+
+    //not-pick
+    f(idx+1,words,score,curScore,freqMpL);
+
+}
+    int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score){
+        map<char,int>mpL;
+        w=words.size();
+        char ch='a';
+        for(auto it:letters)mpL[it]++;
+        for(auto it:score){
+            mpS[ch]=it;
+            ch++;
+        }
+        f(0,words,score,0,mpL);
+        return maxScore;
+    }
+};
