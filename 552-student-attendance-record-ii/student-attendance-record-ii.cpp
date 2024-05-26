b00041ec87729378c52510d3f89cd03a41388c25
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-05-26 10:37:42 +0530

    Time: 195 ms (61.18%) | Memory: 19.1 MB (76.09%) - LeetSync

---
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+int dp[100001][2][3];
+int MOD=1e9+7;
+
+int f(int absCnt,int latCnt,int idx){
+    if(absCnt==2  ||  latCnt==3)return 0;
+    if(idx==0)return 1;
+
+    if(dp[idx][absCnt][latCnt]!=-1)return dp[idx][absCnt][latCnt];
+
+    int ans=0;
+    ans+=f(absCnt,0,idx-1);ans=ans%MOD;
+    ans+=f(absCnt+1,0,idx-1);ans=ans%MOD;
+    ans+=f(absCnt,latCnt+1,idx-1);ans=ans%MOD;
+
+    return dp[idx][absCnt][latCnt]=ans%MOD;
+}
+    int checkRecord(int n){
+        memset(dp,-1,sizeof(dp));
+        return f(0,0,n);
+    }
+};
