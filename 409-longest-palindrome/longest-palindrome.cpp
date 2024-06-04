0b9ad743296eaca41ab298f2d4c410561389084f
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-06-04 06:23:12 +0530

    Time: 3 ms (55.35%) | Memory: 8.9 MB (48.94%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -6,9 +6,7 @@ public:
          for(auto it:s)mp[it]++;
          int cnt=0;int flag=0;
          //now
-         for(auto it:mp){
-                if(it.second%2==0)cnt+=it.second;
-            }
+            for(auto it:mp)if(it.second%2==0)cnt+=it.second;
             for(auto it:mp){
                 if(it.second%2!=0){
                     cnt+=((it.second)/2)*(2);
@@ -16,7 +14,7 @@ public:
                 }
             }
             for(auto it:mp){
-                if(it.second==1 || flag){
+                if(flag){
                     cnt++;
                     break;
                 }
