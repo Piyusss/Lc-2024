8bcee2116bc496d46c3c12c8ba854cd0f92f1622
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-06-12 14:06:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (34.14%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -4,10 +4,9 @@ public:
         for(int i=0 ; i<nums.size()-1 ; i++){
             for(int j=i+1 ; j<nums.size() ; j++){
                 if(nums[j]<nums[i]){
-                    int a=nums[i];
-                    int b=nums[j];
-                    nums[i]=b;
-                    nums[j]=a;
+                    nums[i]=nums[i]^nums[j];
+                    nums[j]=nums[j]^nums[i];
+                    nums[i]=nums[i]^nums[j];
                 }
             }
         }
