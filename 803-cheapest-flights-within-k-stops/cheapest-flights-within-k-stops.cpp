572ecb5ad8f5842cac1f1d1a3cac08e480bf9dc1
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-06-30 22:16:50 +0530

    Time: 3 ms (99.97%) | Memory: 17.1 MB (48.72%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -5,25 +5,27 @@ public:
         for(auto it:flights){
             adj[it[0]].push_back({it[1],it[2]});
         }
+
         queue<pair<int,pair<int,int>>>q;
-        q.push({0,{src,0}});
+        q.push({-1,{src,0}});
+
         vector<int>dist(n,1e9);
         dist[src]=0;
 
         while(!q.empty()){
-            auto it=q.front();
+            int stops=q.front().first;
+            int node=q.front().second.first;
+            int cost=q.front().second.second;
+
             q.pop();
+            
+            if(stops==k)continue;
 
-            int stops=it.first;
-            int node=it.second.first;
-            int cost=it.second.second;
-
-            if(stops>k)continue;
             for(auto it:adj[node]){
                 int adjNode=it.first;
                 int edW=it.second;
 
-                if(cost+edW<dist[adjNode] && stops<=k){
+                if(cost+edW<dist[adjNode]){
                     dist[adjNode]=cost+edW;
                     q.push({stops+1,{adjNode,cost+edW}});
                 }
