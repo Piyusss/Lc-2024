ab3de25be7b6c9fcf85406dbf5369d5d0768d93b
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-06-30 21:52:43 +0530

    Time: 273 ms (8.40%) | Memory: 69.3 MB (7.03%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        priority_queue<pair<int,pair<int,int>>,
-        vector<pair<int,pair<int,int>>>,
-        greater<pair<int,pair<int,int>>>>pq;
+    vector<vector<int>>dir={{0,1},{0,-1},{-1,0},{1,0}};
+    int minimumEffortPath(vector<vector<int>>& heights){
         int n=heights.size();
         int m=heights[0].size();
 
+        priority_queue<
+                pair<int,pair<int,int>>,
+                vector<pair<int,pair<int,int>>>,
+                greater<pair<int,pair<int,int>>>
+        >pq;
+        pq.push({0,{0,0}});
+
         vector<vector<int>>dist(n,vector<int>(m,1e9));
         dist[0][0]=0;
-        pq.push({0,{0,0}});
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
 
         while(!pq.empty()){
-            auto it=pq.top();
+
+            int diff=pq.top().first;
+            int row=pq.top().second.first;
+            int col=pq.top().second.second;
+
             pq.pop();
 
-            int diff=it.first;
-            int row=it.second.first;
-            int col=it.second.second;
-
-            if(row==n-1&& col==m-1)return diff;
-
-            for(int i=0;i<4;i++){
-                int nr=row+dr[i];
-                int nc=col+dc[i];
+            for(auto it:dir){
+                int nr=row+it[0];
+                int nc=col+it[1];
 
                 if(nr>=0 && nc>=0 && nr<=n-1 && nc<=m-1){
                     int newEffort=max(abs(heights[nr][nc]-heights[row][col]),diff);
                     if(newEffort<dist[nr][nc]){
                         dist[nr][nc]=newEffort;
-                        pq.push({newEffort,{nr,nc}});
+                        pq.push({dist[nr][nc],{nr,nc}});
                     }
                 }
             }
         }
-        return 0;
+        return dist[n-1][m-1];
     }
 };
