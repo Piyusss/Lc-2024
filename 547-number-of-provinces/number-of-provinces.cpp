1005d027e323da6fa2583d2afec4551c5903fb8c
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-06-30 15:29:00 +0530

    Time: 18 ms (80.88%) | Memory: 18.1 MB (74.79%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,34 +1,55 @@
-class Solution {
-    private:
-    void dfs(vector<int>adjLs[],vector<int>&vis,int node){
-        vis[node]=1;
-        for(auto it:adjLs[node]){
-            if(!vis[it]){
-                dfs(adjLs,vis,it);
-            }
+class DisjointSet{
+    vector<int>rank,parent;
+    public:
+    DisjointSet(int n){
+        rank.resize(n+1);
+        parent.resize(n+1);
+        for(int i=0 ; i<=n ; i++){
+            parent[i]=i;
         }
     }
+    int findUPar(int node){
+        if(node==parent[node])return node;
+        return parent[node]=findUPar(parent[node]);
+    }
+    void unionByRank(int u,int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v)return;
+            if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            }
+            else if(rank[ulp_v]<rank[ulp_u]){
+                parent[ulp_v]=ulp_u;
+            }
+            else {
+                parent[ulp_v]=ulp_u;
+                rank[ulp_u]++;
+            }
+    }
+};
+
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& adjMat) {
+        //number of components will be equal to , number of unique ultimate-parents
+        //or, simply the number of nodes which have ultimate parents as themselves.
+
         int n=adjMat.size();
-        vector<int>adjLs[n+1];
-        
-        for(int i=0 ; i<n ; i++){
-            for(int j=0 ; j<n ; j++){
-                if(adjMat[i][j]==1 && i!=j){
-                    adjLs[i+1].push_back(j+1);
-                    adjLs[j+1].push_back(i+1);
+        DisjointSet ds(n);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(adjMat[i][j]==1){
+                    ds.unionByRank(i+1,j+1);
                 }
             }
         }
-        vector<int>vis(n+1);
+        //now
         int cnt=0;
-        for(int i=1 ; i<=n ; i++){
-            if(!vis[i]){
-                cnt++;
-                dfs(adjLs,vis,i);
-            }
+        for(int i=1;i<=n;i++){
+            if(ds.findUPar(i)==i)cnt++;
         }
-        return cnt;   
+        return cnt;
     }
 };
