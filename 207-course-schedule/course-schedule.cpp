a33a024bc6e63003231740db1c1a50e62dbfc1fe
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-01 13:36:45 +0530

    Time: 20 ms (28.34%) | Memory: 17.3 MB (61.70%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-bool dfs(int node,vector<int>& vis,vector<int>adjList[],
-vector<int>&pathVis){
-    vis[node]=1;
-    pathVis[node]=1;
-
-    for(auto it:adjList[node]){
-        if(!vis[it]){
-            if(dfs(it,vis,adjList,pathVis))return 1;
-        }
-        else{
-            if(pathVis[it])return 1;
-        }
-    }
-
-    pathVis[node]=0;
-    return 0;
-}
     bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int>indegree(n);
+
         vector<int>adjList[n];
-        
         for(auto it:pre){
             adjList[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
         }
 
-        vector<int>vis(n),pathVis(n);
+        queue<int>q;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                if(dfs(i,vis,adjList,pathVis))return 0;
+            if(indegree[i]==0)q.push(i);
+        }
+
+        vector<int>ans;
+        while(!q.empty()){
+            int node=q.front();
+            ans.push_back(node);
+            q.pop();
+
+            for(auto it:adjList[node]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
             }
         }
 
-        return 1;
+        if(ans.size()==n)return 1;
+        return 0;
+
     }
 };
