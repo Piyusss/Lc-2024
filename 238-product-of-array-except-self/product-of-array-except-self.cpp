34fe85bd4df16220e1c7f0dc0614641cf79aa4ed
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-01 21:14:42 +0530

    Time: 24 ms (77.87%) | Memory: 38 MB (88.15%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,33 +2,26 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums){
         int n=nums.size();
-        vector<int>ans;
+
         int pr=1;
+        for(int i=0;i<n;i++)pr*=nums[i];
+
+        vector<int>noOfZeros;
+        int productWithoutZeros=1;
         for(int i=0;i<n;i++){
-            pr*=nums[i];
+            if(nums[i]==0)noOfZeros.push_back(i);
+            else productWithoutZeros*=nums[i];
         }
-        //now
-        vector<int>flag;
-        int nzp=1;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0)flag.push_back(i);
-            if(nums[i]!=0)nzp*=nums[i];
-        }
-        if(pr!=0){
+        if(pr){
             for(int i=0;i<n;i++)nums[i]=pr/nums[i];
             return nums;
         }
-        else{
-            for(int i=0;i<n;i++){
-                if(nums[i]!=0){
-                    nums[i]=0;
-                }
-                else{
-                    if(flag.size()>1)nums[i]=0;
-                    else nums[i]=nzp;
-                }
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0)nums[i]=0;
+            else{
+                if(noOfZeros.size()>1)nums[i]=0;
+                else nums[i]=productWithoutZeros;
             }
-            return nums;
         }
         return nums;
     }
