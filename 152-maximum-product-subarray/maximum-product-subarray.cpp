2c5f295e697495fd7c1e2b9fa69cd39dc87f4f0f
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-01 23:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (14.48%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -5,16 +5,12 @@ public:
 
         int pr=1,pr2=1,maxi=INT_MIN;
         for(int i=0;i<n;i++){
-            if(pr>=1e9){
-                pr=1;continue;
-            }
+            if(pr>=1e9 || pr<=-1e9)pr=1;
             pr*=nums[i];
             maxi=max(maxi,pr);
             if(pr==0)pr=1;
 
-            if(pr2>=1e9){
-                pr2=1;continue;
-            }
+            if(pr2>=1e9 || pr2<=-1e9)pr2=1;
             pr2*=nums[n-i-1];
             maxi=max(maxi,pr2);
             if(pr2==0)pr2=1;
