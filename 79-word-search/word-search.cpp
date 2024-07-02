b76873ce1d03570998d08ed181e620f7d5aca603
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-02 13:52:01 +0530

    Time: 233 ms (79.26%) | Memory: 9.8 MB (72.26%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
 vector<vector<int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
-bool dfs(vector<vector<char>>&board,int n,int m,int i,int j,string word,int idx){
-    if(idx==word.length())return 1;
-    if(i<0 || j<0 || i>=n || j>=m || board[i][j]=='$' || board[i][j]!=word[idx])return 0;
+int n,m;
+
+bool dfs(vector<vector<char>>&board,int i,int j,string &word,int idx){
+    if(idx==word.length()-1)return 1;
 
     char temp=board[i][j];
     board[i][j]='$';
+
     for(auto &it:dir){
         int new_i=i+it[0];
         int new_j=j+it[1];
-        if(dfs(board,n,m,new_i,new_j,word,idx+1))return 1;
+        
+        if(new_i>=0 && new_j>=0 && new_i<n && new_j<m && board[new_i][new_j]!='$' && board[new_i][new_j]==word[idx+1]){
+            if(dfs(board,new_i,new_j,word,idx+1))return 1;
+        }
     }
     board[i][j]=temp;
     return 0;
 }
     bool exist(vector<vector<char>>& board, string word) {
-        int n=board.size();
-        int m=board[0].size();
+        n=board.size();
+        m=board[0].size();
         //now
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]==word[0]){
-                    if(dfs(board,n,m,i,j,word,0))return 1;
+                    if(dfs(board,i,j,word,0))return 1;
                 }
             }
         }
