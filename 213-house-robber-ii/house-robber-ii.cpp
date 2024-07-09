04b1eff1c4b448b9d73d4472962b863af807b2ef
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-09 15:38:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.77%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -3,14 +3,12 @@ public:
 
 int dp[100+1];
 
-int f(vector<int>&nums,int idx){
-    if(idx==0)return nums[0];
-    if(idx<0)return 0;
-
-    if(dp[idx]!=-1)return dp[idx];
-
-    return dp[idx]=max(nums[idx]+f(nums,idx-2),f(nums,idx-1));
-}
+    int f(vector<int>& nums) {
+        int n=nums.size();
+        dp[0]=nums[0];
+        for(int i=1;i<=n-1;i++)dp[i]=max(i>=2 ? nums[i]+dp[i-2] : nums[i] , dp[i-1]);
+        return dp[n-1];
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(nums.size()==1)return nums[0];
@@ -22,9 +20,9 @@ int f(vector<int>&nums,int idx){
         }
 
         memset(dp,-1,sizeof(dp));
-        int a=f(temp1,n-2);
+        int a=f(temp1);
         memset(dp,-1,sizeof(dp));
-        int b=f(temp2,n-2);
+        int b=f(temp2);
         return max(a,b);
     }
 };
