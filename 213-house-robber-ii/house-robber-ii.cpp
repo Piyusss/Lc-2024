c4df4d68960b6919c93e8c4c92740670941862f2
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-09 15:35:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (69.18%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-int f(vector<int>& nums) {
-        int n=nums.size();
-        int prev=nums[0];int prev2=0;
-        for(int i=1;i<=n-1;i++){
-            int pick=nums[i];if(i>1)pick+=prev2;
-            int notPick=0+prev;
-            int cur=max(pick,notPick);
-            prev2=prev;
-            prev=cur;
-        }
-        return prev;
-    }
 
-    int rob(vector<int>& nums){
+int dp[100+1];
+
+int f(vector<int>&nums,int idx){
+    if(idx==0)return nums[0];
+    if(idx<0)return 0;
+
+    if(dp[idx]!=-1)return dp[idx];
+
+    return dp[idx]=max(nums[idx]+f(nums,idx-2),f(nums,idx-1));
+}
+    int rob(vector<int>& nums) {
         int n=nums.size();
         if(nums.size()==1)return nums[0];
+
         vector<int>temp1,temp2;
         for(int i=0;i<nums.size();i++){
             if(i!=0)temp1.push_back(nums[i]);
             if(i!=nums.size()-1)temp2.push_back(nums[i]);
         }
-        return max(f(temp1),f(temp2));
+
+        memset(dp,-1,sizeof(dp));
+        int a=f(temp1,n-2);
+        memset(dp,-1,sizeof(dp));
+        int b=f(temp2,n-2);
+        return max(a,b);
     }
 };
