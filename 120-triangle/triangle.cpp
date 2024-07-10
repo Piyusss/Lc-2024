8853682ca31703bd0f3327393a6b8fda01e94bf6
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-10 17:10:49 +0530

    Time: 8 ms (27.32%) | Memory: 11.4 MB (58.89%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+
     int minimumTotal(vector<vector<int>>&grid){
         int n=grid.size();
         vector<vector<int>>dp=grid;
@@ -8,11 +9,9 @@ public:
                 dp[i][j]=0;
             }
         }
-        //base-case-in-tabulation
-        for(int i=0;i<grid[n-1].size();i++){
-            dp[n-1][i]=grid[n-1][i];
-        }
-        //now
+        
+        for(int i=0;i<grid[n-1].size();i++)dp[n-1][i]=grid[n-1][i];
+
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<grid[i].size();j++){
                 int down=grid[i][j]+dp[i+1][j];
@@ -20,6 +19,7 @@ public:
                 dp[i][j]=min(down,diag);
             }
         }
+
         return dp[0][0];
     }
 };
