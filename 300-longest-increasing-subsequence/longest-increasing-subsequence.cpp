b32ebb30417d1c2717fa182624713c87f9fcd3c9
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-10 21:09:51 +0530

    Time: 68 ms (67.69%) | Memory: 13.1 MB (43.82%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -4,16 +4,37 @@ public:
         int n=nums.size();
 
         int maxi=1;
-        vector<int>dp(n+1,1);
+        vector<int>dp(n+5,1);
+        vector<int>hash(n+1);
+        int lastIdx=0;
 
         for(int i=0;i<n;i++){
+            hash[i]=i;
             for(int j=0;j<i;j++){
                 if(nums[i]>nums[j]){
-                    dp[i]=max(dp[i],1+dp[j]);
+                    if(1+dp[j]>dp[i]){
+                        dp[i]=1+dp[j];
+                        hash[i]=j;
+                        lastIdx=i;
+                    }
                 }
             }
             maxi=max(maxi,dp[i]);
         }
+
+        
+//PRINT
+        vector<int>temp;
+		temp.push_back(nums[lastIdx]);
+        while(hash[lastIdx]!=lastIdx){
+    		lastIdx=hash[lastIdx];
+    		temp.push_back(nums[lastIdx]);
+    	}
+    	reverse(temp.begin(),temp.end());
+        for(auto it:temp)cout<<it<<" ";
+        cout<<endl;
+//PRINT
+
         return maxi;
     }
 };
