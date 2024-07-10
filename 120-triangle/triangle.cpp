1099f17cca2ec3235c8ed82b78017d5445f797b8
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-10 17:11:29 +0530

    Time: 4 ms (73.13%) | Memory: 11.4 MB (58.89%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -3,20 +3,15 @@ public:
 
     int minimumTotal(vector<vector<int>>&grid){
         int n=grid.size();
+
         vector<vector<int>>dp=grid;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<dp[i].size();j++){
-                dp[i][j]=0;
-            }
-        }
+        for(int i=0;i<n;i++)for(int j=0;j<dp[i].size();j++)dp[i][j]=0;
         
         for(int i=0;i<grid[n-1].size();i++)dp[n-1][i]=grid[n-1][i];
 
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<grid[i].size();j++){
-                int down=grid[i][j]+dp[i+1][j];
-                int diag=grid[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(down,diag);
+                dp[i][j]=min(grid[i][j]+dp[i+1][j],grid[i][j]+dp[i+1][j+1]);
             }
         }
 
