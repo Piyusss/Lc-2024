c6a8e9ce5937f3db4b47ca544a23b9f19ddaf770
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-12 15:50:41 +0530

    Time: 4 ms (48.71%) | Memory: 17 MB (5.07%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+void f(int ind,vector<int>ds,int n,vector<int>nums,set<vector<int>>&store){
+    store.insert(ds);
+    if(ind==n){
+        return;
+    }
+
+    f(ind+1,ds,n,nums,store);
+
+    ds.push_back(nums[ind]);
+    f(ind+1,ds,n,nums,store);
+    ds.pop_back();
+
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ds;
+        set<vector<int>>store;
+        sort(nums.begin(),nums.end());
+        f(0,ds,n,nums,store);
+        vector<vector<int>>final;
+        for(auto it:store)final.push_back(it);
+        return final;
+    }
+};
