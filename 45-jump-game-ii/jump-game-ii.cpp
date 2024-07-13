d80df3f2c01870d6aac55afbf86a5e443f0a6c2f
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-13 17:11:15 +0530

    Time: 64 ms (27.08%) | Memory: 19.4 MB (31.05%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -3,23 +3,20 @@ public:
 
 int dp[10000+2];
 
-int f(int idx,vector<int>& nums,int n){
-    if(idx>=n-1)return 0;
-
-    if(dp[idx]!=-1)return dp[idx];
-
-    int mini=n+1;
-    for(int i=1;i<=nums[idx];i++){
-        int jumps=1+f(idx+i,nums,n);
-        mini=min(mini,jumps);
-    }
-
-    return dp[idx]=mini;
-}
     int jump(vector<int>& nums) {
         int n=nums.size();
         if(n==1)return 0;
-        memset(dp,-1,sizeof(dp));
-        return f(0,nums,n);
+        memset(dp,0,sizeof(dp));
+        
+        for(int idx=n-2;idx>=0;idx--){
+            int mini=n+1;
+            for(int i=1;i<=nums[idx];i++){
+                int jumps=1+(idx+i<=n-1 ? dp[idx+i] : 0);
+                mini=min(mini,jumps);
+            }
+           dp[idx]=mini;
+        }
+
+        return dp[0];
     }
 };
