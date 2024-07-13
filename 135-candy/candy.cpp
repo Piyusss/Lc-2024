a0a3803e2349a619d26effbd608f152392145fdb
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-13 15:14:43 +0530

    Time: 15 ms (55.54%) | Memory: 19.8 MB (77.33%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     int candy(vector<int>& nums){
         int n=nums.size();
+        int sum=1,i=1;
 
-        vector<int>left(n);int cur=1;
-        left[0]=1;
-
-        for(int i=1;i<n;i++){
-            if(nums[i]>nums[i-1])left[i]=left[i-1]+1;
-            else left[i]=1;
-        }
-
-        int cnt=max(1,left[n-1]);
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]>nums[i+1]){
-                left[i]=max(left[i],++cur);
+        while(i<n){
+            if(nums[i]==nums[i-1]){
+                sum++;i++;
+                continue;
             }
-            else{
-                cur=1;
-                left[i]=max(left[i],cur);
-            }
-            cnt+=left[i];
-        }
 
-        return cnt;
+            int peak=1;
+            while(i<n && nums[i]>nums[i-1]){
+                peak++;sum+=peak;i++;
+            }
+
+            int down=1;
+            while(i<n && nums[i]<nums[i-1]){
+                sum+=down;
+                down++;i++;
+            }
+            if(down>peak)sum+=down-peak;
+        }
+        return sum;
     }
 };
