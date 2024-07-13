9c88bdb3e387af4a939573ef90d4ee5237e61a3f
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-13 17:04:23 +0530

    Time: 48 ms (42.63%) | Memory: 51.2 MB (18.91%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-
-int dp[10000+2];
-
-bool f(int idx,vector<int>& nums,int n){
-    if(idx==n-1)return 1;
-    if(idx>=n)return 0;
-
-    if(dp[idx]!=-1)return dp[idx];
-
-    for(int i=1;i<=nums[idx];i++){
-        int new_idx=idx+i;
-        if(f(new_idx,nums,n))return dp[new_idx]=1;
-    }
-    return dp[idx]=0;
-}
-    bool canJump(vector<int>& nums){
+    bool canJump(vector<int>& nums) {
         int n=nums.size();
-        if(n==1)return 1;
-        memset(dp,-1,sizeof(dp));
-        return f(0,nums,n);
+
+        int maxi=0;
+
+        for(int i=0;i<n;i++){
+            if(maxi<i)return 0;
+            maxi=max(maxi,i+nums[i]);
+        }
+
+        return 1;
     }
 };
