26855dffc2dc89cc5a34538cf473b37e9dab9192
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-13 19:30:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (8.17%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -5,11 +5,11 @@ public:
         for(int i=0;i<32;i++){
             int lasta=a&1,lastb=b&1,lastc=c&1;
 
-            if(lastc==0){
-                if(lasta!=0 && lastb!=0)cnt+=2;
-                else if(lasta==1 || lastb==1)cnt++;
+            if(!lastc){
+                if(lasta && lastb)cnt+=2;
+                else if(lasta || lastb)cnt++;
             }
-            else if(lasta==0 && lastb==0)cnt++;
+            else if(!lasta && !lastb)cnt++;
             a>>=1;b>>=1;c>>=1;
         }
         return cnt;
