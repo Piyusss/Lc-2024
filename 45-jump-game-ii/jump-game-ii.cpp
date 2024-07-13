aa78b9967079d59b9985e28ba5ec16ecae48f7e9
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-13 18:49:04 +0530

    Time: 4 ms (95.84%) | Memory: 19.2 MB (31.22%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-
-int dp[10000+2];
-
     int jump(vector<int>& nums) {
         int n=nums.size();
-        if(n==1)return 0;
-        memset(dp,0,sizeof(dp));
-        
-        for(int idx=n-2;idx>=0;idx--){
-            int mini=n+1;
-            for(int i=1;i<=nums[idx];i++){
-                int jumps=1+(idx+i<=n-1 ? dp[idx+i] : 0);
-                mini=min(mini,jumps);
-            }
-           dp[idx]=mini;
-        }
 
-        return dp[0];
+        int l=0,r=0,jumps=0;
+
+        while(r<n-1){
+            int farthest=0;
+            for(int i=l;i<=r;i++)farthest=max(farthest,i+nums[i]);
+            l=r+1;
+            r=farthest;
+            jumps++;
+        }
+        
+        return jumps;
     }
 };
