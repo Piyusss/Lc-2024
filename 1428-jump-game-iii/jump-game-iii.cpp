35d6776c15d6cb6ad6b274014352f872edbc700c
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-13 17:40:38 +0530

    Time: 29 ms (81.84%) | Memory: 36.8 MB (56.13%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
 
-void f(int idx,vector<int>& nums,int n,vector<int> &vis){
+int vis[50000+2];
+
+void f(int idx,vector<int>& nums,int n){
     if(idx<0 || idx>=n || vis[idx])return;
 
     vis[idx]=1;
-    f(idx+nums[idx],nums,n,vis);
-    f(idx-nums[idx],nums,n,vis);
+    f(idx+nums[idx],nums,n);
+    f(idx-nums[idx],nums,n);
 
 }
     bool canReach(vector<int>& nums, int start) {
         int n=nums.size();
-        vector<int>vis(n,0);
-        f(start,nums,n,vis);
+        memset(vis,0,sizeof(vis));
+        f(start,nums,n);
 
         for(int i=0;i<n;i++)if(nums[i]==0 && vis[i])return 1;
         return 0;
