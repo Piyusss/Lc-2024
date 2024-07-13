140cdb338f14386eec69196017f297ffaf5f7758
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-13 14:34:20 +0530

    Time: 19 ms (24.08%) | Memory: 20.4 MB (21.73%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -3,8 +3,8 @@ public:
     int candy(vector<int>& nums){
         int n=nums.size();
 
-        vector<int>left(n),right(n);
-        left[0]=right[n-1]=1;
+        vector<int>left(n);int cur=1;
+        left[0]=1;
 
         for(int i=1;i<n;i++){
             if(nums[i]>nums[i-1]){
@@ -13,16 +13,23 @@ public:
             else left[i]=1;
         }
 
+        left[n-1]=max(left[n-1],1);
         for(int i=n-2;i>=0;i--){
             if(nums[i]>nums[i+1]){
-                right[i]=right[i+1]+1;
+                // right[i]=right[i+1]+1;
+                left[i]=max(left[i],cur+1);
+                cur++;
             }
-            else right[i]=1;
+            else{
+                // right[i]=1;
+                cur=1;
+                left[i]=max(left[i],cur);
+            }
         }
 
         int cnt=0;
 
-        for(int i=0;i<n;i++)cnt+=max(left[i],right[i]);
+        for(int i=0;i<n;i++)cnt+=left[i];
 
         return cnt;
     }
