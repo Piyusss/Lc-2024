d485be64ff8b716cf78018a0e0fb751db155d845
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-15 15:00:32 +0530

    Time: 51 ms (14.75%) | Memory: 10.3 MB (81.81%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
 
-int dp[501][501];
+int dp[502][502];
 
-int f(int i,int j,string &s,string &t){
-    if(i<0)return j+1; // INSERT IN S
-    if(j<0)return i+1; // DELETE IN S
-
-    if(dp[i][j]!=-1)return dp[i][j];
-
-    if(s[i]==t[j])return dp[i][j]=f(i-1,j-1,s,t);
-    return dp[i][j]=1+min(f(i-1,j,s,t),min(f(i,j-1,s,t),f(i-1,j-1,s,t))); //DELETE , INSERT , REPLACE
-
-}
     int minDistance(string s, string t) {
         int n=s.size();
         int m=t.size();
-        memset(dp,-1,sizeof(dp));
+        memset(dp,0,sizeof(dp));
 
-        return f(n-1,m-1,s,t);
+        for(int j=0;j<=m;j++)dp[0][j]=j;
+        for(int i=1;i<=n;i++)dp[i][0]=i;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){   
+            if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1];
+            else dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1])); //DELETE , INSERT , REPLACE
+            }
+        }
+
+        return dp[n][m];
     }
 };
