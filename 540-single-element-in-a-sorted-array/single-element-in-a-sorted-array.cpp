7d3b0f06704833073ceda97bbcbba6561d410b47
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-15 19:08:13 +0530

    Time: 14 ms (88.93%) | Memory: 25 MB (6.15%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -11,7 +11,7 @@ public:
         int r=n-2;
 
         while(l<=r){
-            int mid=(l+r)/2;
+            int mid=l + (r-l)/2;
 
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])return nums[mid];
 
