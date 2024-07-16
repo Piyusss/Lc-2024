2b250ba7dee96916fd9a1367bd7f908d4353510f
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-16 16:51:54 +0530

    Time: 7 ms (36.88%) | Memory: 9.4 MB (62.84%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        int n=s.size();
-        vector<string>ans;
-        //now
-        string temp="";
-        for(int i=0;i<n;i++){
-            if(s[i]!=' '){
-                temp+=s[i];
-                if(i==n-1)ans.push_back(temp);
-            }
-            else {
-                if(temp.size()>=1)ans.push_back(temp);
-                temp="";
-            }
-        }
-        reverse(ans.begin(),ans.end());
-        //now
+    string reverseWords(string s){
+
+        reverse(s.begin(),s.end());
+        
+        stringstream ss(s);
+
         string res="";
-        for(int i=0;i<=ans.size()-1;i++){
-            if(i<ans.size()-1){
-                res+=ans[i];
-                res+=" ";
-            }
-            else res+=ans[i];
+        string token="";
+
+        while(ss>>token){
+            reverse(token.begin(),token.end());
+            res+=token+" ";
         }
+
+        res.pop_back();
+
         return res;
     }
-};
+}; 
