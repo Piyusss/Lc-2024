05f5951f58a25c320a4f4406f11e55e875a7e12f
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-16 17:23:34 +0530

    Time: 1206 ms (7.32%) | Memory: 8.2 MB (76.69%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
+
 bool isPal(string &s,int i,int j){
-    while(i<=j)if(s[i++]!=s[j--])return 0;
-    return 1;
+    while(i<=j){
+        if(s[i++]!=s[j--])return 0;
+    }
+            return 1;
 }
     string longestPalindrome(string s) {
         int n=s.size();
-        int start=0,max_len=0;
-        for(int i=0;i<n;i++){
+        
+        int max_len=0;
+        int sp=0;
+         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                if(s[i]==s[j] && (j-i+1)>max_len && isPal(s,i,j)){
-                    max_len=j-i+1;
-                    start=i;
+                if(isPal(s,i,j)){
+                    if(max_len<j-i+1){
+                        max_len=j-i+1;
+                        sp=i;
+                    }
                 }
             }
-        }
-        return s.substr(start,max_len);
+         }
+         return s.substr(sp,max_len);
     }
 };
