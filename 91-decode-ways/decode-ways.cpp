44a334afc5cf63510402a76e671ff85743e3e62f
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-19 19:10:21 +0530

    Time: 4 ms (31.06%) | Memory: 8.1 MB (51.70%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+int dp[101];
+
+int f(string &s,int &n,int idx){
+    if(idx==n)return 1;
+    if(idx>n ||  s[idx]=='0')return 0;
+
+    if(dp[idx]!=-1)return dp[idx];
+
+    int res=f(s,n,idx+1);
+    if(s[idx]=='1' || ( idx+1<=n-1 && s[idx]=='2' && s[idx+1]<='6' ))res+=f(s,n,idx+2);
+
+    return dp[idx]= res;
+}
+    int numDecodings(string s){
+        int n=s.size();
+        memset(dp,-1,sizeof(dp));
+        return f(s,n,0);
+        
+    }
+};
