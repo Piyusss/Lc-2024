2b42ab96b7535ed06d15c67be5a7abc5e3f4e408
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-19 21:13:27 +0530

    Time: 19 ms (39.99%) | Memory: 7.7 MB (90.86%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -4,21 +4,20 @@ public:
 int dp[30+1][1000+1];
 int M=1E9+7;
 
-int f(int nthDice,int k,int target){
-    if(target<0)return 0;
-    if(target==0 && nthDice==0)return 1;
-    if(target!=0 && nthDice==0)return 0;
-
-    if(dp[nthDice][target]!=-1)return dp[nthDice][target];
-
-    int cnt=0;
-    for(int face=1;face<=k;face++){
-        cnt= (cnt+f(nthDice-1,k,target-face))%M;
-    }
-    return dp[nthDice][target]= cnt%M;
-}
     int numRollsToTarget(int n, int k, int target){
-        memset(dp,-1,sizeof(dp));
-        return f(n,k,target)%M;
+        memset(dp,0,sizeof(dp));
+        
+        dp[0][0]=1;
+
+        for(int nthDice=1;nthDice<=n;nthDice++){
+            for(int t=1;t<=target;t++){
+                int cnt=0;
+                for(int face=1;face<=k;face++){
+                    if(t-face>=0)cnt= (cnt+dp[nthDice-1][t-face])%M;
+                }
+                dp[nthDice][t]= cnt%M;
+            }
+        }
+        return dp[n][target];
     }
 };
