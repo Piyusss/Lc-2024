0a51f51b732068d4c2df39e55729c1a0c1d4e1f4
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-19 19:20:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (51.70%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -3,21 +3,20 @@ public:
 
 int dp[101];
 
-int f(string &s,int &n,int idx){
-    if(idx==n)return 1;
-    if(idx>n ||  s[idx]=='0')return 0;
-
-    if(dp[idx]!=-1)return dp[idx];
-
-    int res=f(s,n,idx+1);
-    if(s[idx]=='1' || ( idx+1<=n-1 && s[idx]=='2' && s[idx+1]<='6' ))res+=f(s,n,idx+2);
-
-    return dp[idx]= res;
-}
     int numDecodings(string s){
         int n=s.size();
-        memset(dp,-1,sizeof(dp));
-        return f(s,n,0);
+        memset(dp,0,sizeof(dp));
         
+        dp[n]=1;
+
+        for(int idx=n-1;idx>=0;idx--){
+            if(s[idx]=='0')continue;
+
+            int res=dp[idx+1];
+            if( ( idx+1<=n-1 && s[idx]=='1') || ( idx+1<=n-1 && s[idx]=='2' && s[idx+1]<='6' ))res+=dp[idx+2];
+            dp[idx]= res;
+        }
+    
+        return dp[0];
     }
 };
