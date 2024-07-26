47d3aad5e34f46bf62895ceb43e3be10704a34e9
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-27 01:13:24 +0530

    Time: 403 ms (58.72%) | Memory: 171.2 MB (61.71%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,38 +1,82 @@
-class Solution {
-public:
-
-void dfs(int node,vector<int>adjList[],vector<int>& vis,long long &flag){
-    vis[node]=1;
-    flag++;
-
-    for(auto it:adjList[node]){
-        if(!vis[it]){
-            dfs(it,adjList,vis,flag);
+class DisjointSet {
+    public:
+    vector<int> rank, parent, sizee;
+    int flag;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        sizee.resize(n + 1);
+        flag=n;
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            sizee[i] = 1;
         }
     }
-}
-    long long countPairs(int n, vector<vector<int>>& edges) {
-        vector<int>adjList[n];
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+        flag--;
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (sizee[ulp_u] < sizee[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            sizee[ulp_v] += sizee[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            sizee[ulp_u] += sizee[ulp_v];
+        }
+        flag--;
+    }
+    bool isSingleComponentLeftOrNot(){
+        return flag==1;
+    }
+};
+
+class Solution {
+public:
+    long long countPairs(int n, vector<vector<int>>& edges){
+        DisjointSet ds(n);
 
         for(auto it:edges){
             int u=it[0];
             int v=it[1];
-            adjList[u].push_back(v);
-            adjList[v].push_back(u);
+            ds.unionBySize(u,v);
         }
-        vector<int>vis(n,0);
 
-        long long ans=0;
-        long long remaining_nodes=n;
+        unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                long long flag=0;
-                dfs(i,adjList,vis,flag);
-                ans+= (flag*(remaining_nodes-flag));
-                remaining_nodes-=flag;
-            }
+            mp[ds.findUPar(i)]++;
         }
 
+        long long temp=n;
+        long long ans=0;
+        for(auto &it:mp){
+            ans+= (it.second*(temp-it.second));
+            temp-=it.second;
+        }
         return ans;
     }
 };
