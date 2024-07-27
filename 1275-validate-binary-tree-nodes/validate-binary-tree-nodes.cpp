44d7d41891796d70700741908281621fac40f470
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-27 20:13:59 +0530

    Time: 32 ms (74.56%) | Memory: 36.2 MB (58.73%) - LeetSync

--- 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
+++ 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
@@ -1,49 +1,69 @@
+class DisjointSet {
+    public:
+    vector<int> rank, parent, sizee;
+    int flag;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        sizee.resize(n + 1,1);
+        flag=n;
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            sizee[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+        flag--;
+    }
+
+    bool unionBySize(int u, int v,int child_ka_parent,int parent_ka_parent ) {
+
+        if (child_ka_parent != v)
+            return false;
+
+        if (parent_ka_parent == child_ka_parent)
+            return false;
+
+        parent[v]=u;
+        flag--;
+        return true;
+    }
+    bool isSingleComponentLeftOrNot(){
+        return flag==1;
+    }
+};
+
+
+
 class Solution {
 public:
-
-void dfs(int node,vector<int>adjList[],vector<int>& vis,vector<int>& cnt){
-    vis[node]=1;
-    cnt[0]++;
-
-    for(auto &it:adjList[node]){
-        if(!vis[it])dfs(it,adjList,vis,cnt);
-    }
-}
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild){
+        DisjointSet ds(n);
 
-        vector<int>adjList[n+1];
-        unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
-            int left_Child=leftChild[i];
-            int right_Child=rightChild[i];
-
-            if(left_Child!=-1){
-                adjList[i].push_back(left_Child);
-                if(mp.find(left_Child)!=mp.end())return 0;
-                mp[left_Child]=i;
-            }
-            if(right_Child!=-1){
-                adjList[i].push_back(right_Child);
-                if(mp.find(right_Child)!=mp.end())return 0;
-                mp[right_Child]=i;
-            }
+            if(leftChild[i]!=-1 && !ds.unionBySize(i,leftChild[i],ds.findUPar(leftChild[i]),ds.findUPar(i))) return 0;
+            if(rightChild[i]!=-1 && !ds.unionBySize(i,rightChild[i],ds.findUPar(rightChild[i]),ds.findUPar(i))) return 0;
         }
-
-        int noOfRoots=0;
-        int root=-1;
-        for(int i=0;i<n;i++){
-            if(mp.find(i)==mp.end()){
-                noOfRoots++;
-                root=i;
-            }
-        }
-        if(noOfRoots>1)return 0;
-        if(root==-1)return 0;
-
-        vector<int>cnt(1,0);
-        vector<int>vis(n,0);
-        dfs(root,adjList,vis,cnt);
-
-        return cnt[0]==n;
+        return ds.isSingleComponentLeftOrNot();
     }
 };
