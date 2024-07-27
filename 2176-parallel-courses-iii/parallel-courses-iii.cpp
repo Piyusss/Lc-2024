0939ae43292cce98a9448548b88878208c5bf5c1
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-07-27 16:23:09 +0530

    Time: 330 ms (29.25%) | Memory: 146.8 MB (45.33%) - LeetSync

--- 2176-parallel-courses-iii/parallel-courses-iii.cpp
+++ 2176-parallel-courses-iii/parallel-courses-iii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
-        vector<int>adjList[n+1];
-        vector<int>inDegree(n+1,0);
-        vector<int>timeCopy(n+1,0);
+        vector<int>adjList[n];
+        vector<int>inDegree(n,0);
+        vector<int>timeCopy(n,0);
 
 
         for(auto it:relations){
-            int u=it[0];
-            int v=it[1];
+            int u=it[0]-1;
+            int v=it[1]-1;
             adjList[u].push_back(v);
             inDegree[v]++;
         }
 
         queue<int>q;
-        for(int i=1;i<=n;i++){
+        for(int i=0;i<n;i++){
             if(inDegree[i]==0){
                 q.push(i);
-                timeCopy[i]=time[i-1];
+                timeCopy[i]=time[i];
             }
         }
 
@@ -26,7 +26,7 @@ public:
             q.pop();
 
             for(auto it:adjList[node]){
-                timeCopy[it]=max(timeCopy[it],timeCopy[node]+time[it-1]);
+                timeCopy[it]=max(timeCopy[it],timeCopy[node]+time[it]);
                 inDegree[it]--;
                 if(inDegree[it]==0)q.push(it);
             }
