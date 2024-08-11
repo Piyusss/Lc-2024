52eed266172144b7bf58f10ab7236eb673f61e48
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-08-11 11:01:45 +0530

    Time: 69 ms (63.22%) | Memory: 24.8 MB (58.91%) - LeetSync

---
+++ 1691-minimum-number-of-days-to-disconnect-island/minimum-number-of-days-to-disconnect-island.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+
+vector<vector<int>>dir={{0,1},{0,-1},{-1,0},{1,0}};
+int n,m;
+
+void dfs(int i,int j,vector<vector<int>>&vis,vector<vector<int>>& grid){
+    vis[i][j]=1;
+
+    for(auto &it:dir){
+        int ni=i+it[0];
+        int nj=j+it[1];
+
+        if(ni>=0 && ni<n && nj>=0 && nj<m && !vis[ni][nj] && grid[ni][nj]==1){
+            dfs(ni,nj,vis,grid);
+        }
+    }
+}
+    int minDays(vector<vector<int>>& grid) {
+        n=grid.size();
+        m=grid[0].size();
+
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int mark=0;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]==1){
+                    dfs(i,j,vis,grid);
+                    mark=1;
+                    cnt++;
+                }
+            }
+        }
+
+        if(cnt>1 || cnt==0)return 0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    grid[i][j]=0;
+                    int cnt2=0;
+                    vector<vector<int>>vis2(n,vector<int>(m,0));
+                    for(int k=0;k<n;k++){
+                        for(int l=0;l<m;l++){
+                            if(!vis2[k][l] && grid[k][l]==1){
+                                dfs(k,l,vis2,grid);
+                                cnt2++;
+                            }
+                        }
+                    }
+                    if(cnt2>1 || cnt2==0)return 1;
+                    grid[i][j]=1;
+                }
+            }
+        }
+
+        return 2;
+    }
+};
