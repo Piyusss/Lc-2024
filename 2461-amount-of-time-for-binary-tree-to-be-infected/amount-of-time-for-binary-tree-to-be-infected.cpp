d9abdfe6a112bb79bbb4bba91802ff958c27bd0a
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-08-14 23:05:26 +0530

    Time: 276 ms (69.62%) | Memory: 162.9 MB (34.16%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,40 +1,85 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    unordered_map<int, vector<int>> graph;
-    void f(TreeNode* root) {
-        if (!root) return;
-        if (root->left) {
-            graph[root->val].push_back(root->left->val);
-            graph[root->left->val].push_back(root->val);
+
+void markParent(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&parent_track){
+    queue<TreeNode*>q;
+    q.push(root);
+
+    while(!q.empty()){
+        TreeNode* current=q.front();
+        q.pop();
+
+        if(current->left){
+            parent_track[current->left]=current;
+            q.push(current->left);
         }
-        if (root->right) {
-            graph[root->val].push_back(root->right->val);
-            graph[root->right->val].push_back(root->val);
+        if(current->right){
+            parent_track[current->right]=current;
+            q.push(current->right);
         }
-        f(root->left);
-        f(root->right);
     }
-        int amountOfTime(TreeNode* root, int start) {
-        f(root);
+}
 
-        queue<pair<int,int>> q; q.push({start,0});
-        unordered_set<int> visited; visited.insert(start);
+TreeNode* target=new TreeNode(-1);
 
-        int maxi=0;
-        while(!q.empty()) {
-                int currentNode = q.front().first;
-                int step=q.front().second;
+void f(TreeNode* root,int start){
+    if(root==NULL)return;
+
+    if(root->val==start){
+        target=root;
+        return;
+    }
+    f(root->left,start);
+    f(root->right,start);
+}
+
+    int amountOfTime(TreeNode* root, int start) {
+        unordered_map<TreeNode*,TreeNode*>parent_track;
+        markParent(root,parent_track);
+        
+        f(root,start);
+
+        unordered_map<TreeNode*,bool>visited;
+        queue<TreeNode*>q;
+        q.push(target);
+        visited[target]=1;
+        int cur_level=-1;
+        int time=0;
+
+        while(!q.empty()){
+            int n=q.size();
+
+            for(int i=0;i<n;i++){
+                TreeNode* current=q.front();
                 q.pop();
-
-                maxi=max(maxi,step);
-
-                for(int adjacentNode:graph[currentNode]) {
-                    if(!visited.count(adjacentNode)){
-                        visited.insert(currentNode);
-                        q.push({adjacentNode,step+1});
-                    }
+                if(current->left && !visited[current->left]){
+                    q.push(current->left);
+                    visited[current->left]=1;
                 }
+                if(current->right && !visited[current->right]){
+                    q.push(current->right);
+                    visited[current->right]=1;
+                }
+                if(parent_track[current] && !visited[parent_track[current]]){
+                    q.push(parent_track[current]);
+                    visited[parent_track[current]]=1;
+                }
+            }
+            time++;
         }
-        return maxi;
+
+
+        return time-1;
     }
 };
