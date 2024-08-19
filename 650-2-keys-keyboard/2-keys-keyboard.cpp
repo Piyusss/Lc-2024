04e808ac97f89c9cae61c358b64931ec31a1e601
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-08-19 20:04:18 +0530

    Time: 9 ms (46.95%) | Memory: 11.8 MB (25.96%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+int dp[1001][1001];
+
+int f(int num,int copy,int n){
+    if(num==n)return 0;
+    if(num>n)return INT_MAX-30;
+
+    if(dp[num][copy]!=-1)return dp[num][copy];
+
+    int copied=2+f(2*num,num,n);
+    int notCopied=1+f(num+copy,copy,n);
+
+    return dp[num][copy]=min(copied,notCopied);
+}
+    int minSteps(int n){
+        if(n==1)return 0;
+        memset(dp,-1,sizeof(dp));
+        return 1+f(1,1,n);
+    }
+};
