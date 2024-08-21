7299ef7501182e4a76673d3156d65f2427809253
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-08-22 00:56:07 +0530

    Time: 128 ms (60.87%) | Memory: 38 MB (67.67%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -2,30 +2,19 @@ class Solution {
 public:
 
 int dp[601][101][101];
-int si;
+int si,n,m;
 
-int f(int idx,vector<string>& strs,int n,int m,int one,int zero){
+int f(int idx,vector<string>& strs,int one,int zero){
     if(idx==si)return 0;
 
     if(dp[idx][one][zero]!=-1)return dp[idx][one][zero];
 
-    int curOne=0,curZero=0;
-    string s=strs[idx];
-    for(int i=0;i<s.size();i++){
-        if(s[i]=='0')curZero++;
-        else curOne++;
-    }
-
-    int pick=0;
-    if(one+curOne<=n && zero+curZero<=m) pick=1+f(idx+1,strs,n,m,one+curOne,zero+curZero);
-    int not_pick=f(idx+1,strs,n,m,one,zero);
-
-    return dp[idx][one][zero] = max(pick,not_pick);
+    return dp[idx][one][zero] = max((one+strs[idx].size()-count(strs[idx].begin(),strs[idx].end(),'0')<=n && zero+count(strs[idx].begin(),strs[idx].end(),'0')<=m ? 1+f(idx+1,strs,one+strs[idx].size()-count(strs[idx].begin(),strs[idx].end(),'0'),zero+count(strs[idx].begin(),strs[idx].end(),'0')) : 0),f(idx+1,strs,one,zero));
     
 }
-    int findMaxForm(vector<string>& strs, int m, int n){
-        si=strs.size();
+    int findMaxForm(vector<string>& strs, int M, int N){
+        n=N;m=M;si=strs.size();
         memset(dp,-1,sizeof(dp));
-        return f(0,strs,n,m,0,0);
+        return f(0,strs,0,0);
     }
 };
