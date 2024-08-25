b528c3ed9bd13e8818830cfcb4f1d29423ecd2b6
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-08-25 09:55:20 +0530

    Time: 6 ms (57.30%) | Memory: 8.5 MB (80.43%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+double dp[105][105];
+
+double f(int poured,int i,int j){
+    if(i<0 || j<0 || i<j)return 0.0;
+    if(i==0 && j==0)return poured;
+
+    if(dp[i][j]!=-1.0)return dp[i][j];
+
+    double up_left=(f(poured,i-1,j-1)-1)/2.0;if(up_left<0)up_left=0.0;
+    double up_right=(f(poured,i-1,j)-1)/2.0;if(up_right<0)up_right=0.0;
+
+    return dp[i][j]=up_left + up_right;
+}
+    double champagneTower(int poured, int i, int j) {
+        for(int i=0;i<101;i++){
+            for(int j=0;j<101;j++){
+                dp[i][j]=-1.0;
+            }
+        }
+
+        return min(1.0,f(poured,i,j));
+    }
+};
