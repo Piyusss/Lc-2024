6743d6f5eac9ea05eb641a3d08adb59d8778625d
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-08-25 15:44:00 +0530

    Time: 50 ms (21.73%) | Memory: 426.8 MB (33.26%) - LeetSync

--- 826-soup-servings/soup-servings.cpp
+++ 826-soup-servings/soup-servings.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
 
 vector<vector<int>>options={{100,0},{75,25},{50,50},{25,75}};
-vector<vector<double>>dp;
 
-double f(int soupALeft,int soupBLeft){
+double f(int soupALeft,int soupBLeft,vector<vector<double>>& dp){
     if(soupALeft<=0 && soupBLeft<=0)return 0.5;
     if(soupALeft<=0)return 1.0;
     if(soupBLeft<=0)return 0.0;
@@ -15,14 +14,14 @@ double f(int soupALeft,int soupBLeft){
     for(auto &it:options){
         int A=it[0];
         int B=it[1];
-        res+= f(soupALeft-A,soupBLeft-B);
+        res+= f(soupALeft-A,soupBLeft-B,dp);
     }
 
     return dp[soupALeft][soupBLeft]= 0.25*res;
 }
     double soupServings(int n){
         if(n>=5000)return 1;
-        dp.resize(n+1,vector<double>(n+1,-1.0));
-        return f(n,n);
+        vector<vector<double>>dp(n+1,vector<double>(n+1,-1.0));
+        return f(n,n,dp);
     }
 };
