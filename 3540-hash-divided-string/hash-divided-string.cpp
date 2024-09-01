dadb72e5695f64f83f5f7bb9021c3a62e0da7902
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-09-01 06:11:27 +0530

    Time: 4 ms (100.00%) | Memory: 11.8 MB (10.00%) - LeetSync

--- 3540-hash-divided-string/hash-divided-string.cpp
+++ 3540-hash-divided-string/hash-divided-string.cpp
@@ -8,14 +8,8 @@ public:
         for(int i=0;i<n;i+=k){
             string temp=s.substr(i,k);
             int cnt=0;
-            for(int i=0;i<temp.size();i++){
-                cnt+=temp[i]-'a';
-            }
-            cnt=cnt%26;
-            char ch=cnt+'a';
-            string flag="";
-            flag+=ch;
-            ans+=flag;
+            for(int i=0;i<temp.size();i++) cnt=(cnt+(temp[i]-'a'))%26;
+            ans+=(cnt+'a');
         }
 
         return ans;
