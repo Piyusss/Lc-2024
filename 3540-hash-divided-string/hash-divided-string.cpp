12c2e0d4d268e8e2b111b34cac45fb7845e9e657
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-09-01 06:13:16 +0530

    Time: 8 ms (70.00%) | Memory: 12 MB (10.00%) - LeetSync

--- 3540-hash-divided-string/hash-divided-string.cpp
+++ 3540-hash-divided-string/hash-divided-string.cpp
@@ -3,12 +3,13 @@ public:
     string stringHash(string s, int k) {
         int n=s.size();
 
-        string ans="";
+        string ans="",temp;
+        int cnt=0;
 
         for(int i=0;i<n;i+=k){
-            string temp=s.substr(i,k);
-            int cnt=0;
-            for(int i=0;i<temp.size();i++) cnt=(cnt+(temp[i]-'a'))%26;
+            cnt=0;
+            temp=s.substr(i,k);
+            for(int i=0;i<k;i++) cnt=(cnt+(temp[i]-'a'))%26;
             ans+=(cnt+'a');
         }
 
