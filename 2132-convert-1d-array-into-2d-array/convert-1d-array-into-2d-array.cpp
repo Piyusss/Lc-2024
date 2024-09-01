09b33c81fdcb784601ecf0f6e7765c15fbc58b72
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-09-01 05:47:06 +0530

    Time: 87 ms (39.10%) | Memory: 87.4 MB (99.82%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -6,16 +6,16 @@ public:
 
         int k=0;
 
-        vector<vector<int>>ans2(m,vector<int>(n,0));
+        ans.resize(m);
+        for(auto &row:ans)row.resize(n);
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                ans2[i][j]=original[k];
-                k++;
+                ans[i][j]=original[k++];
             }
         }
 
-        return ans2;
+        return ans;
         
     }
 };
