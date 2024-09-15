4675ef4f39d273343bb27b7982ead32884b03406
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-09-15 23:10:19 +0530

    Time: 75 ms (53.95%) | Memory: 18.8 MB (58.84%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -5,16 +5,19 @@ public:
 
         unordered_map<int,int>mp;
         mp[0]=-1;
-        int res=0;
-        int bitmask=0;
 
+        int res=0,bitmask=0;
         for(int i=0;i<n;i++){
-            s[i]=='a' ? (bitmask=bitmask ^ 1<<0) : (s[i]=='e' ? (bitmask=bitmask ^ 1<<1) : (s[i]=='i' ? (bitmask=bitmask ^ 1<<2) : (s[i]=='o' ? (bitmask=bitmask ^ 1<<3) : (s[i]=='u' ? (bitmask=bitmask ^ 1<<4) : (bitmask=bitmask)))));
+            s[i]=='a'  ? (bitmask=bitmask ^ 1<<0) :
+            (s[i]=='e' ? (bitmask=bitmask ^ 1<<1) :
+            (s[i]=='i' ? (bitmask=bitmask ^ 1<<2) :
+            (s[i]=='o' ? (bitmask=bitmask ^ 1<<3) :
+            (s[i]=='u' ? (bitmask=bitmask ^ 1<<4) : (0==0)))));
 
-            if(mp.find(bitmask)!=mp.end())res=max(res,i-mp[bitmask]);
-            else mp[bitmask]=i;
+            mp.find(bitmask)!=mp.end() ? res=max(res,i-mp[bitmask]) : mp[bitmask]=i;
         }
 
         return res;
+        
     }
 };
