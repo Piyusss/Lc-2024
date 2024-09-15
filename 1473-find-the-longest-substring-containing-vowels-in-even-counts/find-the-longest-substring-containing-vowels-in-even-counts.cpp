131ae4135ac38b5c63ed4f77b84e567548bf418f
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-09-15 23:07:50 +0530

    Time: 77 ms (51.86%) | Memory: 19 MB (32.79%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -9,11 +9,7 @@ public:
         int bitmask=0;
 
         for(int i=0;i<n;i++){
-            if(s[i]=='a') bitmask=bitmask ^ (1<<0);
-            else if(s[i]=='e') bitmask=bitmask ^ (1<<1);
-            else if(s[i]=='i') bitmask=bitmask ^ (1<<2);
-            else if(s[i]=='o') bitmask=bitmask ^ (1<<3);
-            else if(s[i]=='u') bitmask=bitmask ^ (1<<4);
+            s[i]=='a' ? (bitmask=bitmask ^ 1<<0) : (s[i]=='e' ? (bitmask=bitmask ^ 1<<1) : (s[i]=='i' ? (bitmask=bitmask ^ 1<<2) : (s[i]=='o' ? (bitmask=bitmask ^ 1<<3) : (s[i]=='u' ? (bitmask=bitmask ^ 1<<4) : (bitmask=bitmask)))));
 
             if(mp.find(bitmask)!=mp.end())res=max(res,i-mp[bitmask]);
             else mp[bitmask]=i;
