1c4b0af09066a0acc1e16885396a7df96b856f9d
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-09-16 09:21:39 +0530

    Time: 31 ms (5.83%) | Memory: 18.5 MB (25.23%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
+
+int mini=INT_MAX;
+
+void f(int n,vector<string>&nums,int i,int j){
+    if(j==-1)j=0;
+    int h1=stoi(nums[i].substr(0,2));
+    int m1=stoi(nums[i].substr(3,2));
+    int h2=stoi(nums[j].substr(0,2));
+    int m2=stoi(nums[j].substr(3,2));
+
+    int dif=abs((h1*60+m1)-(h2*60+m2));
+    mini=min(mini,min(dif,1440-(dif)));
+}
     int findMinDifference(vector<string>& nums){
         int n=nums.size();
 
         sort(nums.begin(),nums.end());
         for(auto it:nums)cout<<it<<" ";
 
-        int mini=INT_MAX;
-        for(int i=1;i<n;i++){
-            int h1=stoi(nums[i].substr(0,2));
-            int m1=stoi(nums[i].substr(3,2));
-            int h2=stoi(nums[i-1].substr(0,2));
-            int m2=stoi(nums[i-1].substr(3,2));
-
-            int dif=abs((h1*60+m1)-(h2*60+m2));
-            mini=min(mini,min(dif,1440-(dif)));
-        }
-
-        int h1=stoi(nums[0].substr(0,2));
-        int m1=stoi(nums[0].substr(3,2));
-        int h2=stoi(nums[n-1].substr(0,2));
-        int m2=stoi(nums[n-1].substr(3,2));
-
-        int dif=abs((h1*60+m1)-(h2*60+m2));
-        mini=min(mini,min(dif,1440-(dif)));
+        for(int i=1;i<n;i++) f(n,nums,i,i-1);
+        f(n,nums,n-1,-1);
 
         return mini;
     }
