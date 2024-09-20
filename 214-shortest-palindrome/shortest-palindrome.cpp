aca4847e618211678d35a9a9fd5484f793ddc64b
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-09-21 00:02:17 +0530

    Time: 7 ms (64.09%) | Memory: 9.8 MB (87.62%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+
+    string shortestPalindrome(string s) {
+        int n=s.size();
+
+        string t1="",t2="",copy=s;
+        reverse(copy.begin(),copy.end());
+
+        for(int i=0;i<n;i++){
+            if(memcmp(s.c_str(),copy.c_str()+i,n-i)==0) return copy.substr(0,i) + s;
+        }
+        
+        return "";
+    }
+};
+//aaacecaa
