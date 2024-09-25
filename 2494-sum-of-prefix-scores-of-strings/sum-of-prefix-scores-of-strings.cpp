741fac49d3f94fea7cc5827f724651f3a4eae75e
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-09-25 08:00:52 +0530

    Time: 552 ms (73.56%) | Memory: 704.1 MB (71.07%) - LeetSync

---
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,46 @@
+class TrieNode {
+public:
+    TrieNode* children[26]; 
+    int count; 
+
+    TrieNode(){
+        count=0;
+        for(int i=0;i<26;i++) {
+            children[i]=NULL;
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<int> sumPrefixScores(vector<string>& words){
+        TrieNode* root=new TrieNode();
+
+        for (auto &word:words) {
+            TrieNode* node=root;
+            for (char &c:word) {
+                int idx=c-'a';
+                if(node->children[idx]==NULL) {
+                    node->children[idx]=new TrieNode();
+                }
+                node=node->children[idx];
+                node->count++; 
+            }
+        }
+
+        vector<int>ans;
+        for(auto &word:words){
+            TrieNode* node=root;
+            int score=0;
+            for(char &c:word){
+                int idx=c-'a';
+                node=node->children[idx];
+                score+=node->count; 
+            }
+            ans.push_back(score);
+        }
+
+        return ans;
+
+    }
+};
