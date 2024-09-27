8767fe298dea46e35884215d65d20edc604f2a95
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-09-27 07:18:41 +0530

    Time: 1086 ms (5.00%) | Memory: 37.5 MB (99.90%) - LeetSync

--- 731-my-calendar-ii/my-calendar-ii.cpp
+++ 731-my-calendar-ii/my-calendar-ii.cpp
@@ -10,7 +10,9 @@ public:
     bool book(int start, int end){
         for(int i=0;i<v.size();i++){
             for(int j=i+1;j<v.size();j++){
-                if(start<v[i].second && end>v[i].first && start<v[j].second && end>v[j].first && v[i].first<v[j].second && v[i].second>v[j].first)return 0;
+                if(start<v[i].second && end>v[i].first && 
+                   start<v[j].second && end>v[j].first && 
+                   v[i].first<v[j].second && v[i].second>v[j].first) return 0;
             }
         }
         v.push_back({start,end});
