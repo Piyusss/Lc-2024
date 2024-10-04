f423352a35288465e2bbeaa6f8b40a6730b4774c
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-10-04 08:00:44 +0530

    Time: 67 ms (42.40%) | Memory: 56.5 MB (23.40%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -7,16 +7,15 @@ public:
 
         sort(skill.begin(),skill.end());
 
-        ll sum=skill[0]*skill[n-1];
+        ll sum=0;
         ll req=skill[0]+skill[n-1];
         ll i=1,j=n-2;
 
         while(i<j){
             if(skill[i]+skill[j]!=req)return -1;
-            sum+=skill[i]*skill[j];
-            i++,j--;
+            sum+=skill[i++]*skill[j--];
         }
 
-        return (ll)sum;
+        return (ll)sum+skill[0]*skill[n-1];
     }
 };
