84a2f2b9e00f2e86b522ea9fbc58019ef203887e
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-10-07 14:07:17 +0530

    Time: 13 ms (9.28%) | Memory: 10.1 MB (93.92%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -4,8 +4,7 @@ public:
         int n=s.size();
 
         while(1){
-            bool flag=0;
-            int idx=s.find("AB"),idx2=s.find("CD");
+            int idx=s.find("AB"),idx2=s.find("CD"),flag=0;
             if(idx!=-1) s.erase(idx,2),flag=1;
             else if(idx2!=-1) s.erase(idx2,2),flag=1;
             if(!flag)break;
