4863f7dfd5e762f199a040247bdb8b859afd8530
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-10-09 17:49:39 +0530

    Time: 38 ms (19.62%) | Memory: 15.8 MB (16.03%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.cpp
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -7,9 +7,7 @@ public:
         stack<char>st;
 
         for(int i=0;i<n;i++){
-            if(s[i]=='('){
-                st.push('(');
-            }
+            if(s[i]=='(') st.push('(');
             else{
                 if(i+1<n && s[i+1]==')'){
                     if(!st.empty())st.pop();
