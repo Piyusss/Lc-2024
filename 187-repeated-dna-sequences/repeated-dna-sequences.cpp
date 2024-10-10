0dbbfa652a061c691f96534a7e340b1f1401ebe6
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-10-10 19:39:14 +0530

    Time: 37 ms (83.56%) | Memory: 22.2 MB (82.48%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        int n=s.size();
+
+        unordered_set<string>st,res;
+
+        for(int i=0;i<=n-10;i++){
+            string ext=s.substr(i,10);
+            if(st.find(ext)!=st.end()) res.insert(ext);
+            else st.insert(ext);
+        }
+
+        vector<string>ans;
+        for(auto &it:res) ans.push_back(it);
+        return ans;
+    }
+};
