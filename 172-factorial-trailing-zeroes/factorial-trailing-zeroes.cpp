44fc6fbe79f739cffc2572b78b6a97d46a788de1
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-10-10 20:07:51 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (78.46%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
 
-int NoOfFivesInprimeFactoriz(int n){
-    int cnt=0;
-
-    for(int i=3;i*i<=n;i+=2){
-        while(n%i == 0){
-            if(i%5==0) cnt++;
-            n/=i;
-        }
-    }
-
-    return cnt + (n%5==0 ? 1 : 0);
+int f(int n){
+    return n/5 + trailingZeroes(n/5);
 }
-
     int trailingZeroes(int n){
-        if(n<=4) return 0;
+        if(n<=4)return 0;
 
-        int cnt=0;
-
-        for(int i=5;i<=n;i++){
-            if(i%5==0){
-                cnt+= NoOfFivesInprimeFactoriz(i);
-            }
-        }
-
-        return cnt;
+        return f(n);
     }
 };
+
+// 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 105 110 115 120 125
+// 25 50 75 100 125
+// 125
