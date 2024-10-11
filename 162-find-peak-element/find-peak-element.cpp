46d90d3e286f27cef67cc7b2aadcbc55c5e9b3d9
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-10-11 19:35:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (86.33%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -4,10 +4,22 @@ public:
         int n=nums.size();
         if(n==1) return 0;
 
-        for(int i=1;i<n-1;i++){
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]) return i;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+
+        int l=1;
+        int r=n-2;
+        int mid=(l+r)/2;
+
+        while(l<=r){
+            mid=(l+r)/2;
+
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
+            else if(nums[mid]>nums[mid-1]) l=mid+1;
+            else r=mid-1;
         }
 
-        return nums[1]>nums[0] ? n-1 : 0;
+        return -1;
+        
     }
 };
