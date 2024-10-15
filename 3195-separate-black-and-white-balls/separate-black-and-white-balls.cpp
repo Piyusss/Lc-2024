b486aab063b58e167fd5c29f8fd10e587c54512a
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-10-15 09:25:14 +0530

    Time: 147 ms (5.05%) | Memory: 31.8 MB (6.32%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -10,16 +10,13 @@ public:
         ll res=0;
 
         for(ll i=0;i<n;i++){
-            if(s[i]=='1'){
-                pq.push(i);
-            }
+            if(s[i]=='1') pq.push(i);
             else{
                 if(pq.empty()) continue;
 
                 ll it=pq.top();
                 pq.pop();
 
-                swap(s[it],s[i]);
                 res+=abs(it-i);
                 pq.push(i);
             }
