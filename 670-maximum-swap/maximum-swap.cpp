f2fd07d18ead75e691338d75af90879e1e971599
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-10-17 13:13:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (31.77%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -4,14 +4,9 @@ public:
         string s=to_string(num);
         int n=s.size();
 
-        vector<int>v(n,-1);
-        v[n-1]=n-1;
+        vector<int>v(n,n-1);
 
-        for(int i=n-2;i>=0;i--){
-            v[i] = s[i] > s[v[i+1]] ? i : v[i+1];
-        }
-
-        for(auto &it:v) cout<<it<<" ";
+        for(int i=n-2;i>=0;i--) v[i] = s[i] > s[v[i+1]] ? i : v[i+1];
 
         for(int i=0;i<n;i++){
             if(i != v[i] && s[i] != s[v[i]]){
