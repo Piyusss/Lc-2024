68a86b835da7c7364ad5d9eecf9b03df3bb79a63
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-10-18 08:49:20 +0530

    Time: 11 ms (78.65%) | Memory: 10 MB (93.24%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -2,38 +2,26 @@ class Solution {
 public:
 
 int t,n,cnt=0;
-vector<int>res;
 
-bool check(vector<int>& res){
-    int start=res[0];
-    for(int i=1;i<res.size();i++) start = start | res[i];
-    return start == t;
-}
-
-void f(vector<int>&nums,int idx){
+void f(vector<int>&nums,int idx,int res){
     if(idx==n){
-        if(res.size()!=0 && check(res)) cnt++;
+        if(res == t) cnt++;
         return;
     }
 
-    res.push_back(nums[idx]);
-    f(nums,idx+1);
-    res.pop_back();
+    f(nums,idx+1,res | nums[idx]);
+    f(nums,idx+1,res);
 
-    f(nums,idx+1);
 }
 
     int countMaxOrSubsets(vector<int>& nums) {
         n=nums.size();
 
-        int start=nums[0];
-        for(int i=1;i<n;i++) start = start | nums[i];
+        t=nums[0];
+        for(int i=1;i<n;i++) t = t | nums[i];
 
-        t=start;
-
-        cout<<t;
-
-        f(nums,0);
+        int res=0;
+        f(nums,0,res);
 
         return cnt;
         
