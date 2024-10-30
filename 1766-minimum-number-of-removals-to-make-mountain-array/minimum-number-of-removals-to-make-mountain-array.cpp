182abb05dcbf10a5f6a9bef38da80d701cc1476f
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-10-30 18:14:56 +0530

    Time: 56 ms (59.09%) | Memory: 14.7 MB (83.50%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
 void f(vector<int>&nums,int n,vector<int>&dp){
-
+    //LIS_it_izz
     for(int i=0;i<n;i++){
         for(int j=0;j<i;j++){
             if(nums[i]>nums[j]){
@@ -10,25 +10,19 @@ void f(vector<int>&nums,int n,vector<int>&dp){
             }
         }
     }
-
 }
 
     int minimumMountainRemovals(vector<int>& nums) {
         int n=nums.size();
 
-        vector<int>dpLeft(n,1);
-        vector<int>dpRight(n,1);
+        vector<int>dpLeft(n,1),dpRight(n,1);
 
         f(nums,n,dpLeft);
         reverse(nums.begin(),nums.end());
         f(nums,n,dpRight);
-        reverse(dpRight.begin(),dpRight.end());
 
-        for(auto &it:dpLeft) cout<<it<<" ";
-        cout<<endl;
-        for(auto &it:dpRight) cout<<it<<" ";
-        
         int res=INT_MAX;
+        reverse(dpRight.begin(),dpRight.end());
         for(int i=1;i<n-1;i++){
             int mountainArray= dpLeft[i] + dpRight[i] - 1;
             if(mountainArray >= 3 && dpLeft[i]>=2 && dpRight[i]>=2) res=min(res,n-mountainArray);
