f5f70122effa2015e3b32f4a91ec34fad07b40a8
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-11-02 20:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (6.09%) - LeetSync

--- 2580-circular-sentence/circular-sentence.cpp
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -8,9 +8,9 @@ public:
         while(ss>>token) v.push_back(token);
 
         int n=v.size();
-        for(int i=0;i<n-1;i++) if(v[i][v[i].size()-1]!=v[i+1][0]) return 0;
+        for(int i=0;i<n-1;i++) if(v[i].back()!=v[i+1].front()) return 0;
 
-        return v[0][0] == v[n-1][v[n-1].size()-1] ? 1 : 0 ;
+        return s[0] == s[s.size()-1] ? 1 : 0 ;
 
     }
 };
