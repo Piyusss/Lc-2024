7ae91c97658615822d2cab98d3258554f6955af2
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-11-02 08:52:18 +0530

    Time: 3 ms (10.00%) | Memory: 9.1 MB (19.13%) - LeetSync

--- 2580-circular-sentence/circular-sentence.cpp
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool isCircularSentence(string s){
-
+        
         vector<string>v;
         stringstream ss(s);
         string token;
         while(ss>>token) v.push_back(token);
 
         int n=v.size();
-        for(int i=0;i<n-1;i++){
-            string a=v[i];
-            string b=v[i+1];
-            if(a[a.size()-1]!=b[0]) return 0;
-        }
+        for(int i=0;i<n-1;i++) if(v[i][v[i].size()-1]!=v[i+1][0]) return 0;
 
         return v[0][0] == v[n-1][v[n-1].size()-1] ? 1 : 0 ;
 
