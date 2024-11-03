8dffd216a0304b1c8fe51f5e04b6164245db498b
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-11-03 12:55:03 +0530

    Time: 2 ms (15.25%) | Memory: 7.7 MB (55.81%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,3 +1,4 @@
+// Approach-01:
 class Solution {
 public:
     bool rotateString(string s, string goal){
@@ -13,3 +14,74 @@ public:
 
     }
 };
+
+// Approach-02:
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal){
+//         int n=s.size();
+//         int m=goal.size();
+
+//         if(m!=n) return 0;
+
+//         s+=s;
+//         for(int i=0;i<n;i++){
+//             if(s.substr(i,m)==goal) return 1;
+//         }
+
+//         return 0;
+
+//     }
+// };
+
+// Approach-03:
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal){
+//         int n=s.size();
+//         int m=goal.size();
+
+//         if(m<n) return 0;
+
+//         s+=s;
+//         for(int i=0;i<n;i++){
+//             if(s.substr(i,m)==goal) return 1;
+//         }
+
+//         return 0;
+
+//     }
+// };
+
+// Approach-04:
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         int n=s.size();
+
+//         for(int i=0;i<n;i++){
+//             rotate(s.begin(),s.begin()+1,s.end());
+//             if(s==goal) return 1;
+//         }
+
+//         return 0;
+
+//     }
+// };
+
+// Approach-05:
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         int n=s.size();
+
+//         for(int i=0;i<n;i++){
+//             s.push_back(s[0]);
+//             s.erase(0,1);
+//             if(s==goal) return 1;
+//         }
+
+//         return 0;
+
+//     }
+// };
