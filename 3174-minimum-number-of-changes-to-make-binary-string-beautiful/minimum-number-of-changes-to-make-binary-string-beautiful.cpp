90f59cc086d4db0764cbd59d5be8beba479914d6
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-11-05 21:58:49 +0530

    Time: 2 ms (47.87%) | Memory: 12.1 MB (67.52%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -4,9 +4,8 @@ public:
         int n=s.size();
 
         int cnt=0;
-        for(int i=0;i<n-1;i++){
-            (s[i]!=s[i+1]) ? (cnt+=1,s[i+1]=s[i]) : (0==0);
-            i+=1;
+        for(int i=0;i<n-1;i+=2){
+            (s[i]!=s[i+1]) ? (cnt+=1) : (0==0);
         }
 
         return cnt;
