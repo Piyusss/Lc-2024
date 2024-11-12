77b9a245b42cc14edc10f67852e3e111b6e8b056
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-11-12 08:40:29 +0530

    Time: 80 ms (22.13%) | Memory: 97.2 MB (42.91%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -4,23 +4,17 @@ public:
         int n=items.size(),m=queries.size();
         sort(items.begin(),items.end());
 
-        for(auto &it:items) cout<<"("<<it[0]<<" "<<it[1]<<")"<<" ";
-        cout<<endl;
-
         vector<int>res(m,0),maxBeauty(n);
         maxBeauty[0]=items[0][1];
-
         for(int i=1;i<n;i++) maxBeauty[i]=max(maxBeauty[i-1],items[i][1]);
-        for(auto &it:maxBeauty) cout<<it<<" ";
 
         int id=0;
         for(auto &it:queries){
             vector<int>flag={it,INT_MAX};
             auto idx=upper_bound(items.begin(),items.end(),flag);
-            int prevIdx=n-1;
-            
+            int prevIdx=(idx-items.begin())-1;
+
             if(idx != items.end()){
-                prevIdx=(idx-items.begin())-1;
                 if(prevIdx>=0) res[id++]=maxBeauty[prevIdx];
                 else if(prevIdx==-1) res[id++]=0;
             }
