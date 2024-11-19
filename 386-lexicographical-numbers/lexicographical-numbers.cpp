c3adadb90d544ce863cb8b50ece8b8a7252d3801
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-11-19 20:02:34 +0530

    Time: 27 ms (18.74%) | Memory: 30.9 MB (8.33%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,13 +1,79 @@
+struct Node {
+    Node* links[10];
+    int cntEndWith = 0;
+    int cntPrefix = 0;
+    bool flag = false;
+
+    bool containsKey(int idx) {
+        return (links[idx] != NULL); 
+    }
+
+    Node* get(int idx) {
+        return links[idx]; 
+    }
+    
+    void put(int index, Node* node) {
+        links[index] = node;
+    }
+
+    void setEnd()
+    {
+        flag = true;
+    }
+
+    bool isEnd()
+    {
+        return flag;
+    }
+};
+
+class Trie {
+public:
+    Node* root; 
+
+public:
+    Trie() {
+        root = new Node(); 
+    }
+
+    void insert(string word) {
+        Node* node = root; 
+        for (int i = 0; i < word.size(); i++) { 
+            if (!node->containsKey(word[i]-'0')) {
+                node->put(word[i]-'0', new Node()); 
+            }
+            node = node->get(word[i]-'0');
+        }
+        node->setEnd();
+    }
+};
+
 class Solution {
 public:
 
-static bool comp(int a,int b){
-    return to_string(a)<to_string(b);
+vector<int>res;
+
+void dfs(Node* root,int num){
+    if(!root) return;
+
+    if(root->isEnd()) res.push_back(num);
+
+    for(int i=0;i<=9;i++){
+        if(root->containsKey(i)){
+            dfs(root->get(i),num*10+i);
+        }
+    }
+
 }
+
     vector<int> lexicalOrder(int n){
-        vector<int>v;
-        for(int i=1;i<=n;)v.push_back(i++);
-        sort(v.begin(),v.end(),comp);
-        return v;
+        Trie trie;
+        for(int i=1;i<=n;i+=1) trie.insert(to_string(i));
+
+        Node* rooted=trie.root;
+        dfs(rooted,0);
+
+        return res;
+
     }
 };
