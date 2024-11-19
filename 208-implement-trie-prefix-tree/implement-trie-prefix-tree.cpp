e9a36fd7122d62a9b2aeac3a01a94537a8abe16e
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-11-19 18:44:44 +0530

    Time: 17 ms (81.76%) | Memory: 48.8 MB (29.73%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,63 +1,83 @@
-class Trie {
-public:
-struct trieNode{
-    bool isEndOfWord;
-    trieNode* child[26];
+struct Node {
+    Node* links[26];
+    int cntEndWith = 0;
+    int cntPrefix = 0;
+    bool flag = false;
+
+    bool containsKey(char ch) {
+        return (links[ch - 'a'] != NULL); 
+    }
+
+    Node* get(char ch) {
+        return links[ch - 'a']; 
+    }
+
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+
+    void setEnd()
+    {
+        flag = true;
+    }
+
+    bool isEnd()
+    {
+        return flag;
+    }
+
+    void increaseEnd() {
+        cntEndWith++; 
+    }
+
+    void increasePrefix() {
+        cntPrefix++;
+    }
+
+    void deleteEnd() {
+        cntEndWith--; 
+    }
+
+    void reducePrefix() {
+        cntPrefix--; 
+    }
 };
 
-trieNode* getNode(){
-    trieNode* newNode=new trieNode();
-    newNode->isEndOfWord=false;
-    for(int i=0;i<26;i++)
-        newNode->child[i]=NULL;
-    return newNode;
-}
-    trieNode* root;
+class Trie {
+private:
+    Node* root; 
+public:
     Trie() {
-        root=getNode();
+        root = new Node(); 
     }
     
     void insert(string word) {
-        trieNode* crawl=root;
-        for(int i=0;i<word.length();i++){
-            char ch=word[i];
-            int idx=ch-'a';
-
-            if(crawl->child[idx]==NULL){
-                crawl->child[idx]=getNode();
-            }
-            crawl=crawl->child[idx];
+        Node* node = root; 
+        for (int i = 0; i < word.size(); i++) { 
+            if (!node->containsKey(word[i])) node->put(word[i], new Node()); 
+            node = node->get(word[i]);
+            node->increasePrefix(); 
         }
-        crawl->isEndOfWord=true;
+        node->increaseEnd();
+        node->setEnd();
     }
     
     bool search(string word) {
-        trieNode* crawl=root;
-        for(int i=0;i<word.length();i++){
-            char ch=word[i];
-            int idx=ch-'a';
-
-            if(crawl->child[idx]==NULL){
-                return 0;
-            }
-            crawl=crawl->child[idx];
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++){
+            if (!node->containsKey(word[i])) return 0;
+            node = node->get(word[i]);
         }
-        if(crawl->isEndOfWord==true)return true;
-        return 0;
+        return node->isEnd();
     }
     
     bool startsWith(string prefix) {
-        trieNode* crawl=root;
-        for(int i=0;i<prefix.length();i++){
-            char ch=prefix[i];
-            int idx=ch-'a';
-
-            if(crawl->child[idx]==NULL){
-                return 0;
-            }
-            crawl=crawl->child[idx];
+        Node *node = root;
+        for (int i = 0; i < prefix.length(); i++){
+            if (!node->containsKey(prefix[i])) return 0;
+            node = node->get(prefix[i]);
         }
-        return true;
+        return 1;
     }
 };
 
