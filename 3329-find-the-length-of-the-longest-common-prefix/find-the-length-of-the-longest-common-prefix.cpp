a08fdf149ed607ab825c0da3160c71ea3aace419
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-11-19 20:24:15 +0530

    Time: 140 ms (86.68%) | Memory: 169.8 MB (45.85%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,28 +1,79 @@
+struct Node {
+    Node* links[10];
+    int cntEndWith = 0;
+    int cntPrefix = 0;
+    bool flag = false;
+
+    bool containsKey(int idx) {
+        return (links[idx] != NULL); 
+    }
+
+    Node* get(int idx) {
+        return links[idx]; 
+    }
+
+    void put(int idx, Node* node) {
+        links[idx] = node;
+    }
+
+    void setEnd()
+    {
+        flag = true;
+    }
+
+    bool isEnd()
+    {
+        return flag;
+    }
+};
+
+class Trie {
+public:
+    Node* root; 
+
+public:
+    Trie() {
+        root = new Node(); 
+    }
+
+    void insert(string word) {
+        Node* node = root; 
+        for (int i = 0; i < word.size(); i++) { 
+            if (!node->containsKey(word[i]-'0')) {
+                node->put(word[i]-'0', new Node()); 
+            }
+            node = node->get(word[i]-'0');
+        }
+        node->setEnd();
+    }
+
+};
 class Solution {
 public:
-    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        int n=arr1.size();
-        int m=arr2.size();
-        string flag="";
 
-        unordered_map<int,int>mp1;
-        for(int i=0;i<n;i++){
-            string s=to_string(arr1[i]);
-            for(int j=1;j<=s.size();j++){
-                flag=s.substr(0,j);
-                mp1[stoi(flag)]++;
-            }
+int f(string num,Node* rooted){
+    int cnt=0;
+
+    for(auto &it:num){
+        if(rooted->containsKey(it-'0')){
+            cnt+=1;
+            rooted=rooted->get(it-'0');
         }
+        else break;
+    }
+
+    return cnt;
+
+}
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        Trie trie;
+        for(auto &it:arr1) trie.insert(to_string(it));
 
         int maxi=0;
-        for(int i=0;i<m;i++){
-            string s=to_string(arr2[i]);
-            for(int j=1;j<=s.size();j++){
-                flag=s.substr(0,j);
-                if(mp1.find(stoi(flag))!=mp1.end())maxi=max(maxi,(int)flag.size());
-            }
-        }
+        Node* rooted=trie.root;
+        for(auto &it:arr2) maxi=max(maxi,f(to_string(it),rooted));
 
         return maxi;
+
     }
 };
