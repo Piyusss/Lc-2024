0dd565f5dce2a0cbb88e0a8f21bd9a2cb0530119
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2024-11-21 12:34:19 +0530

    Time: 897 ms (51.18%) | Memory: 19.9 MB (40.60%) - LeetSync

--- 212-word-search-ii/word-search-ii.cpp
+++ 212-word-search-ii/word-search-ii.cpp
@@ -1,71 +1,195 @@
+string currentWord;
+
+struct Node {
+    Node* links[26];
+    int cntEndWith = 0;
+    int cntPrefix = 0;
+    bool flag = false;
+
+    bool containsKey(char ch) {
+        return (links[ch - 'a'] != NULL); 
+    }
+
+    Node* get(char ch) {
+        return links[ch - 'a']; 
+    }
+
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+
+    void setEnd()
+    {
+        flag = true;
+    }
+
+    bool isEnd()
+    {
+        return flag;
+    }
+
+    void increaseEnd() {
+        cntEndWith++; 
+    }
+
+    void increasePrefix() {
+        cntPrefix++;
+    }
+
+    void deleteEnd() {
+        cntEndWith--; 
+    }
+
+    void reducePrefix() {
+        cntPrefix--; 
+    }
+
+};
+
+class Trie {
+public:
+    Node* root; 
+
+public:
+    Trie() {
+        root = new Node(); 
+    }
+
+    void insert(string word) {
+        Node* node = root; 
+        for (int i = 0; i < word.size(); i++) { 
+            if (!node->containsKey(word[i])) {
+                node->put(word[i], new Node()); 
+            }
+            node = node->get(word[i]);
+            node->increasePrefix(); 
+        }
+        node->increaseEnd();
+        node->setEnd();
+    }
+
+    bool search(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+
+    bool startsWith(string prefix)
+    {
+        Node *node = root;
+        for (int i = 0; i < prefix.length(); i++)
+        {
+            if (!node->containsKey(prefix[i]))
+            {
+                return false;
+            }
+            node = node->get(prefix[i]);
+        }
+        return true;
+    }
+
+    int countWordsEqualTo(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.size(); i++) { 
+            if (node->containsKey(word[i])) { 
+                node = node->get(word[i]); 
+            }else{
+                return 0;
+            }
+        }
+        return node->cntEndWith; 
+    }
+
+    int countWordsStartingWith(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.size(); i++) { 
+            if (node->containsKey(word[i])) { 
+                node = node->get(word[i]); 
+            }else{
+                return 0; 
+            }
+        }
+        return node->cntPrefix; 
+    }
+
+    void erase(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.size(); i++) { 
+            if (node->containsKey(word[i])) { 
+                node = node->get(word[i]);
+                node->reducePrefix(); 
+            } else {
+                return; 
+            }
+        }
+        node->deleteEnd(); 
+    }
+};
+
+vector<pair<int,int>>directions={{-1,0},{0,-1},{0,1},{1,0}};
+
 class Solution {
 public:
-    vector<string>result;
-    vector<pair<int,int>>directions={{-1,0},{1,0},{0,1},{0,-1}};
-    int r,c;
 
+void dfs(int x, int y, Node *node, vector<vector<char>> &grid, vector<vector<bool>> &vis, string &currentWord, unordered_set<string> &res){
+    int n=grid.size();
+    int m=grid[0].size();
 
-    struct trieNode{
-        bool endOfWord;
-        trieNode* children[26];
-        string word;
-    };
+    vis[x][y]=1;
+    currentWord += grid[x][y];
 
-    trieNode* getNode(){
-        trieNode* temp=new trieNode();
-        temp->endOfWord=false;
-        for(int i=0;i<26;i++)temp->children[i]=NULL;
-        temp->word="";
-        return temp;
-    }
+    if(node->isEnd()) res.insert(currentWord);
 
-    void insert(trieNode* root,string str){
-        trieNode* pCrawl=root;
-        for(char ch:str){
-            if(pCrawl->children[ch-'a']==NULL){
-                pCrawl->children[ch-'a']=getNode();
+    for(auto &it:directions){
+        int newX=x+it.first;
+        int newY=y+it.second;
+
+        if(newX >= 0 && newX < n && newY >= 0 && newY < m && !vis[newX][newY]){
+            char nextChar=grid[newX][newY];
+            if(node->containsKey(nextChar)){
+                dfs(newX,newY,node->get(nextChar),grid,vis,currentWord,res);
             }
-            pCrawl=pCrawl->children[ch-'a'];
         }
-        pCrawl->endOfWord=true;
-        pCrawl->word=str;
     }
 
+    vis[x][y]=0;
+    currentWord.pop_back();
 
+}
 
+    vector<string> findWords(vector<vector<char>>& grid, vector<string>& dictionary) {
+        Trie trie;
+        for(auto &it:dictionary)trie.insert(it);
 
-    void dfs(vector<vector<char>>&board,int i,int j,trieNode* root){
-        if(i<0 || i>=r || j<0 || j>=c || board[i][j]=='$' || root->children[board[i][j]-'a']==NULL)return;
-        root=root->children[board[i][j]-'a'];
-        if(root->endOfWord==true){
-            result.push_back(root->word);
-            root->endOfWord=false;
-        }
-        char temp=board[i][j];
-        board[i][j]='$';
-        for(auto it:directions){
-            int new_i=i+it.first;
-            int new_j=j+it.second;
-            dfs(board,new_i,new_j,root);
-        }
-        board[i][j]=temp;
-    }
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        r=board.size();
-        c=board[0].size();
+        int n=grid.size();
+        int m=grid[0].size();
 
-        trieNode* root=getNode();
-        for(auto it:words){
-            insert(root,it);
-        }
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                char ch=board[i][j];
-                if(root->children[ch-'a']!=NULL){
-                    dfs(board,i,j,root);
+        vector<vector<bool>>visited(n,vector<bool>(m,0));
+        Node* root=trie.root;
+
+        unordered_set<string>res;
+
+        for(int i=0;i<n;i+=1){
+            for(int j=0;j<m;j+=1){
+                char startChar=grid[i][j];
+                if(root->containsKey(startChar)){
+                    currentWord = "";
+                    dfs(i, j, root->get(startChar), grid, visited, currentWord, res);
                 }
             }
         }
-        return result;
+
+        vector<string>final;
+        for(auto &it:res) final.push_back(it);
+
+        return final;
     }
 };
