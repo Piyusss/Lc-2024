79cc29f78587e6d0f93a3f2ceba95c112b6a63a7
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2025-09-15 11:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (39.20%) - LeetSync

---
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    bool f(string &s,unordered_map<char,int>&mp){
+        int n=s.size();
+
+        for(auto &c:s){
+            if(mp[c]) return 0;
+        }
+
+        return 1;
+    }
+
+    int canBeTypedWords(string text, string brokenLetters) {
+        unordered_map<char,int>mp;
+
+        for(auto &c:brokenLetters) mp[c]++;
+        int cnt=0;
+        
+        string w;
+        stringstream ss(text);
+        while(ss>>w){
+            if(f(w,mp)) cnt++;
+        }
+
+        return cnt;
+    }
+};
