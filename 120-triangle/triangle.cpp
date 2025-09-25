a46d736e9d8378b4b05f090cff342fd08d090af7
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2025-09-25 13:02:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (8.23%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -5,9 +5,9 @@ public:
         int n=grid.size();
 
         vector<vector<int>>dp=grid;
-        for(int i=0;i<n;i++)for(int j=0;j<dp[i].size();j++)dp[i][j]=0;
+        for(int i=0;i<n;i++) for(int j=0;j<dp[i].size();j++) dp[i][j]=0;
         
-        for(int i=0;i<grid[n-1].size();i++)dp[n-1][i]=grid[n-1][i];
+        for(int i=0;i<grid[n-1].size();i++) dp[n-1][i]=grid[n-1][i];
 
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<grid[i].size();j++){
