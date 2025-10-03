52c53c5157784872e1bc98ecb627c7473b64f386
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2025-10-03 13:19:09 +0530

    Time: 25 ms (51.62%) | Memory: 139.3 MB (34.19%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -3,18 +3,31 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n=nums.size();
 
+        vector<int>res;
+
         deque<int>dq;
+        dq.push_back(0);
 
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-            while(!dq.empty() && dq.front()<=i-k)dq.pop_front();
-            while(!dq.empty() && nums[i]>nums[dq.back()])dq.pop_back();
+        //Handle for the first window
+        for(int i=1;i<k;i++){
+            while(!dq.empty() && nums[i]>=nums[dq.back()]) dq.pop_back();
+            dq.push_back(i);
+        }
+        res.push_back(nums[dq.front()]);
 
+        //Handle for the remaining windows
+        for(int i=k;i<n;i++){
+            //Eliminate all elements of previous window
+            while(!dq.empty() && dq.front()<=i-k) dq.pop_front();
+
+            //Place new element at pefect position
+            while(!dq.empty() && nums[i]>=nums[dq.back()]) dq.pop_back();
             dq.push_back(i);
 
-            if(i>=k-1)ans.push_back(nums[dq.front()]);
+            //Max element at front
+            res.push_back(nums[dq.front()]);
         }
 
-        return ans;
+        return res;
     }
 };
