3d79c63170c34ff89fce95d9d31b0839b147a98a
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2025-10-07 07:30:17 +0530

    Time: 129 ms (39.83%) | Memory: 114.8 MB (36.16%) - LeetSync

---
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& r){
+        int n=r.size();
+        vector<int>ans(n,1);
+
+        unordered_map<int,int>full;
+        set<int>free;
+
+        for(int d=0;d<n;d++){
+            int x=r[d];
+            
+            if(x==0){
+                free.insert(d);
+                continue;
+            }
+
+            ans[d]=-1;
+
+            if(full.count(x)){
+                auto pos=free.upper_bound(full[x]);
+                if(pos==free.end())return {};
+                ans[*pos]=x;
+                free.erase(pos);
+            }
+            full[x]=d;
+        }
+
+        return ans;
+    }
+};
