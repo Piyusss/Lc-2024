a36503622cb3b04c86e9c3fa48eb7bda97f17c4e
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2025-10-25 09:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.15%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        //1 2 3 4 5 6 7 , 2 3 4 5 6 7 8 , 3 4 5 6 7
-
         int res=0,m=1,r=0;
+
         for(int i=1;i<=n;i++){
-            if((i-1)%7==0){
-                //this i is monday
-                res += m;
-                r=m+1;
-                m++;
-            }
-            else{
-                //this is tue-sun
-                res += r;
-                r++;
-            }
+            if(!((i-1)%7)) r=m+1;
+            res += ((i-1)%7==0 ? m : r)++;
         }
 
         return res;
