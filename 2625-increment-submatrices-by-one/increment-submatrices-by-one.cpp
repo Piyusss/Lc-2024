0ef73c15c537fe51bc18864b142cd8c5e7b64605
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2025-11-14 10:25:29 +0530

    Time: 35 ms (56.43%) | Memory: 86.2 MB (99.06%) - LeetSync

---
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -0,0 +1,19 @@
+#define ll long long
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& q) {
+        vector<vector<int>>a(n,vector<int>(n));
+
+        for(auto &v:q){
+            int x1=v[0],y1=v[1],x2=v[2],y2=v[3];
+            
+            for(int i=x1;i<=x2;i++){
+                a[i][y1]++;
+                if(y2+1<n)a[i][y2+1]--;
+            }
+        }
+
+        for(int i=0;i<n;i++) for(int j=1;j<n;j++) a[i][j]+=a[i][j-1];
+        return a;
+    }
+};
