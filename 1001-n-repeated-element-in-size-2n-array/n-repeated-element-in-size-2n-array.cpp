898065fb46726afcc730909fe82f7fc3c6271540
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-01-02 12:33:06 +0530

    Time: 30 ms (5.38%) | Memory: 35.3 MB (6.44%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -4,9 +4,8 @@ public:
         int n=nums.size();
 
         map<int,int>mp;
-        for(auto &c:nums) mp[c]++;
+        for(auto &c:nums) if(++mp[c]==(n>>1)) return c;
         
-        for(auto &c:nums) if(mp[c]==(n>>1)) return c;
         return -1;
     }
 };
