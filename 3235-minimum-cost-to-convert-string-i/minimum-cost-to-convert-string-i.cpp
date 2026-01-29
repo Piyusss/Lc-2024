b865493fcbd77305f4eac51073137a7dd160767f
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-01-29 10:59:23 +0530

    Time: 63 ms (29.56%) | Memory: 100.3 MB (57.74%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+
+    using ll=long long;
+
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        int n = 26;
-        vector<vector<long long>>dist(n,vector<long long>(n,INT_MAX));
+        int n=26;
+        vector<vector<ll>>dist(n,vector<ll>(n,INT_MAX));
         
-        for(int i=0;i<n;i++)
-            dist[i][i]=0;
+        for(int i=0;i<n;i++)dist[i][i]=0;
         
         for(int i=0;i<original.size();i++){
             
             int src=original[i]-'a';
             int dst=changed[i]-'a';
-            long long wt=cost[i];
+            ll wt=cost[i];
             
             dist[src][dst]=min(dist[src][dst] , wt);
         }
@@ -24,15 +26,17 @@ public:
             }
         }
             
-            long long result = 0;
-            for(int i=0; i<source.length();i++){
-                    int src = source[i]-'a';
-                    int dest = target[i]-'a';
-                    if(src == dest) continue;
-                    if(dist[src][dest] == INT_MAX ) return -1;
-                    else result += (long long)dist[src][dest];
-            }
-            
-            return result;
+        ll res=0;
+        for(int i=0;i<source.length();i++){
+            int src=source[i]-'a';
+            int dest=target[i]-'a';
+
+            if(src==dest) continue;
+
+            if(dist[src][dest]==INT_MAX ) return -1;
+            else res += (ll)dist[src][dest];
+        }
+
+        return res;
     }
 };
