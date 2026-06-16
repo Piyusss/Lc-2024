dd4f16372adc8f66cdf32243c2129b3c2ebae232
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-06-17 00:31:01 +0530

    Time: 88 ms (44.15%) | Memory: 182 MB (38.56%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,38 +1,41 @@
 class LRUCache {
 public:
 
-int n;
-list<int>dll;
-map<int,pair<list<int>::iterator,int>>mp;
-
+    int n;
+    list<int>dll;
+    unordered_map<int,pair<list<int>::iterator,int>>mp;
+    
     LRUCache(int capacity) {
         n=capacity;
     }
-
-    void mru(int key){
-        dll.erase(mp[key].first);
-        dll.push_front(key);
-        mp[key].first=dll.begin();
-    }
     
     int get(int key) {
-        if(mp.find(key)==mp.end()){
-            return -1;
+        if(mp.find(key) != mp.end()){
+            pair<list<int>::iterator,int>p=mp[key];
+
+            dll.erase(p.first);
+            dll.push_front(key);
+            mp[key].first=dll.begin();
+
+            return p.second;
         }
-        mru(key);
-        return mp[key].second;
+        return -1;
     }
     
     void put(int key, int value) {
-        if(mp.find(key)!=mp.end()){
-            mp[key].second=value;
-            mru(key);
+        if(mp.find(key) != mp.end()){
+            pair<list<int>::iterator,int>p=mp[key];
+
+            dll.erase(p.first);
+            dll.push_front(key);
+            mp[key]={dll.begin(),value};
         }
         else{
             dll.push_front(key);
             mp[key]={dll.begin(),value};
             n--;
         }
+
         if(n<0){
             mp.erase(dll.back());
             dll.pop_back();
@@ -40,10 +43,3 @@ map<int,pair<list<int>::iterator,int>>mp;
         }
     }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
