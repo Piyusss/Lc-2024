4ea730cea8d6c600f2ebf3d245879ace7985f02e
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-06-17 22:52:54 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.92%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
-void f(vector<int>&nums){
-    //rotate-left
-    int n=nums.size();
-    int store=nums[0];
-    for(int i=1;i<n;i++){
-        nums[i-1]=nums[i];
-    }
-    nums[n-1]=store;
-}
     int search(vector<int>& nums, int target) {
         int n=nums.size();
 
-        int mini=INT_MAX,cnt=0,fix=n,flag=0;
-        while(fix--){
-            int front=nums[0];
-            if(target==front){
-                flag=1;break;
-            }
-            if(front<mini)mini=front;
-            cnt++;
-            f(nums);
+        for(int i=0;i<n;i++){
+            if(nums[i] == target) return i;
         }
-        return flag ? cnt : -1;
+
+        return -1;
     }
 };
