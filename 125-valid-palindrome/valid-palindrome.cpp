9dd30b41610dc854488b623bbabf6012155a50ca
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-06-18 17:10:22 +0530

    Time: 3 ms (28.99%) | Memory: 10.1 MB (37.94%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string t="";
+
+        for(auto &c:s){
+            if(isalnum(c)) t+=tolower(c);
+        }
+
+        cout<<t;
+
+        int l=0,r=t.size()-1;
+        while(l<r){
+            if(t[l]!=t[r]) return 0;
+            l++;
+            r--;
+        }
+
+        return 1;
+    }
+};
