fe552028b13f52440c91ab5b3ee9c8120e678dcc
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-06-18 22:49:43 +0530

    Time: 4 ms (13.40%) | Memory: 13.9 MB (79.85%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -4,13 +4,13 @@ public:
     int n,m;
     vector<pair<int,int>>dir={{0,1},{0,-1},{-1,0},{1,0}};
 
-    void dfs(int i,int j,vector<vector<int>>&vis,vector<vector<char>>&board){
-        vis[i][j]=1;
+    void dfs(int i,int j,vector<vector<char>>&board){
+        board[i][j]='#';
 
         for(auto &c:dir){
             int ni=i+c.first;
             int nj=j+c.second;
-            if(ni>=0 && ni<=n-1 && nj>=0 && nj<=m-1 && !vis[ni][nj] && board[ni][nj]=='O') dfs(ni,nj,vis,board);
+            if(ni>=0 && ni<=n-1 && nj>=0 && nj<=m-1 && board[ni][nj]=='O') dfs(ni,nj,board);
         }
 
         return;
@@ -20,14 +20,17 @@ public:
         n=board.size();
         m=board[0].size();
 
-        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int j=0;j<m;j++) if(board[0][j]=='O') dfs(0,j,board);
+        for(int j=0;j<m;j++) if(board[n-1][j]=='O') dfs(n-1,j,board);
+        for(int i=0;i<n;i++) if(board[i][0]=='O') dfs(i,0,board);
+        for(int i=0;i<n;i++) if(board[i][m-1]=='O') dfs(i,m-1,board);
 
-        for(int j=0;j<m;j++) if(board[0][j]=='O') dfs(0,j,vis,board);
-        for(int j=0;j<m;j++) if(board[n-1][j]=='O') dfs(n-1,j,vis,board);
-        for(int i=0;i<n;i++) if(board[i][0]=='O') dfs(i,0,vis,board);
-        for(int i=0;i<n;i++) if(board[i][m-1]=='O') dfs(i,m-1,vis,board);
-
-        for(int i=1;i<=n-2;i++) for(int j=1;j<=m-2;j++) if(board[i][j]=='O' && !vis[i][j]) board[i][j]='X';
+        for(int i=0;i<=n-1;i++){
+            for(int j=0;j<=m-1;j++){
+                if(board[i][j]=='O') board[i][j]='X';
+                else if(board[i][j]=='#') board[i][j]='O';
+            }
+        }
 
         return;
     }
