08f189745c3e332a592fe5c59484d55628f7a09f
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-06-18 17:10:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (31.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,11 +3,7 @@ public:
     bool isPalindrome(string s) {
         string t="";
 
-        for(auto &c:s){
-            if(isalnum(c)) t+=tolower(c);
-        }
-
-        cout<<t;
+        for(auto &c:s) if(isalnum(c)) t+=tolower(c);
 
         int l=0,r=t.size()-1;
         while(l<r){
