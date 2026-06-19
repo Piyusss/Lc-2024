d1e625e1a8587238114aad1424095a168bca1285
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-06-19 20:58:41 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (99.38%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-bool isValid(vector<vector<char>>&board,int row,int col,char c){
-    int cnt=0;int cnt1=0;int cnt2=0;
-    for(int i=0;i<9;i++){
-        if(board[i][col]==c)cnt1++;
-        if(board[row][i]==c)cnt2++;
-        if(board[3*(row/3)+i/3][3*(col/3)+(i%3)]==c)cnt++;
-    }
-    if(cnt>1 || cnt1>1 || cnt2>1)return 0;
-    return 1;
-}
-bool f(vector<vector<char>>& board){
-    for(int i=0;i<9;i++){
-        for(int j=0;j<9;j++){
-            if(board[i][j]=='.')continue;
-            if(isValid(board,i,j,board[i][j])==false)return 0;
+
+    bool f2(vector<vector<char>>&board,int row,int col){
+        char c=board[row][col];
+        int cnt1=0,cnt2=0,cnt3=0;
+
+        for(int k=0;k<9;k++){
+            if(board[k][col]==c) cnt1++; //column-validate
+            if(board[row][k]==c) cnt2++; //row-validate
+            if(board[3*(row/3)+(k/3)][3*(col/3)+(k%3)]==c) cnt3++; //sub-box-validate
         }
+        if(cnt1>1 || cnt2>1 || cnt3>1) return 0;
+        return 1;
     }
-    return true;
-}
+
+    bool f(vector<vector<char>>& board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                if(!f2(board,i,j)) return 0;
+            }
+        }
+        return true;
+    }
+
     bool isValidSudoku(vector<vector<char>>& board){
         return f(board);
     }
