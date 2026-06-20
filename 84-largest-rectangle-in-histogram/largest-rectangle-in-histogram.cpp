dcd0b155fd28d495204f2904e1af7fc8a550b171
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-06-20 18:05:13 +0530

    Time: 18 ms (73.77%) | Memory: 79.3 MB (98.39%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
-
-    vector<int>nse,pse;
-
-    void preCalcPse(vector<int>&heights,int n){
-        pse.resize(n);
-        stack<int>st;
-
-        for(int i=0;i<n;i++){
-            while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
-            pse[i]=st.empty()?-1:st.top();
-            st.push(i);
-        }
-    }
-
-    void preCalcNse(vector<int>&heights,int n){
-
-        nse.resize(n);
-        stack<int>st;
-
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
-            nse[i]=st.empty()?n:st.top();
-            st.push(i);
-        }
-    }
-
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
 
+        stack<int>st;
         int maxi=0;
 
-        preCalcPse(heights,n);
-        preCalcNse(heights,n);
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || heights[st.top()]>=heights[i])){
+                int ht=heights[st.top()];
+                st.pop();
 
-        for(int i=0;i<n;i++){
-            int area=heights[i]*(nse[i]-pse[i]-1);
-            maxi=max(maxi,area);
+                int pse=st.empty()?-1:st.top();
+                int nse=i;
+
+                maxi=max(maxi,ht*(nse-pse-1));
+            }
+            st.push(i);
         }
 
         return maxi;
