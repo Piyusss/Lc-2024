4ac3a39366249f5e87d9bd0089f3141f522c33e1
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-07-01 23:42:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (53.09%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int left=0,top=0,bottom=n-1,right=m-1;
-        vector<int>ans;
-        while(top<=bottom && left<=right){
-            for(int i=left;i<=right;i++){
-                ans.push_back(grid[top][i]);
-            }
-            top++;
-            for(int i=top;i<=bottom;i++){
-                ans.push_back(grid[i][right]);
-            }
-            right--;
+        int n=grid.size(), m=grid[0].size();
 
-            //now
+        vector<int>ans;
+        int left=0,top=0,bottom=n-1,right=m-1;
+
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++) ans.push_back(grid[top][i]);
+            top+=1;
+            
+            for(int i=top;i<=bottom;i++) ans.push_back(grid[i][right]);
+            right-=1;
 
             if(top<=bottom){
-                for(int i=right;i>=left;i--){
-                    ans.push_back(grid[bottom][i]);
-                }
-                bottom--;
+                for(int i=right;i>=left;i--) ans.push_back(grid[bottom][i]);
+                bottom-=1;
             }
+
             if(left<=right){
-                for(int i=bottom;i>=top;i--){
-                    ans.push_back(grid[i][left]);
-                }
-                left++;
+                for(int i=bottom;i>=top;i--) ans.push_back(grid[i][left]);
+                left+=1;
             }
         }
+        
         return ans;
     }
 };
