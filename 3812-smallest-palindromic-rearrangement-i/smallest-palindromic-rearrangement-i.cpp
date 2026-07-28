7c209345763eb5c7aded0698ca53060f9e238716
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-07-28 13:52:14 +0530

    Time: 128 ms (19.78%) | Memory: 64.5 MB (72.97%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -3,13 +3,14 @@ public:
     string smallestPalindrome(string s) {
         int n=s.size();
 
-        string t="";
-        for(int i=0;i<(n&1?(n/2+1):n/2);i++) t+=s[i];
-        sort(t.begin(),t.end()-(n&1));
+        for(int i=0;i<(n&1?(n/2+1):n/2);i++) s+=s[i];
+        s.erase(0,n);
+        sort(s.begin(),s.end()-(n&1));
+        cout<<s;
 
-        string final=t;
-        for(int i=(n&1?(t.size()-2):(t.size()-1));i>=0;i--) final+=t[i];
-
-        return final;
+        int m=s.size();
+        for(int i=(n&1?(m-2):(m-1));i>=0;i--) s+=s[i];
+        
+        return s;
     }
 };
