f3426e3a36cef1e7d3a2c21a1f0ee4535958dfd8
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-07-28 13:53:38 +0530

    Time: 100 ms (28.79%) | Memory: 73.9 MB (43.74%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -4,13 +4,11 @@ public:
         int n=s.size();
 
         for(int i=0;i<(n&1?(n/2+1):n/2);i++) s+=s[i];
-        s.erase(0,n);
-        sort(s.begin(),s.end()-(n&1));
-        cout<<s;
+        sort(s.begin()+n,s.end()-(n&1));
 
         int m=s.size();
-        for(int i=(n&1?(m-2):(m-1));i>=0;i--) s+=s[i];
-        
-        return s;
+        for(int i=(n&1?(m-2):(m-1));i>=n;i--) s+=s[i];
+
+        return s.substr(n);
     }
 };
