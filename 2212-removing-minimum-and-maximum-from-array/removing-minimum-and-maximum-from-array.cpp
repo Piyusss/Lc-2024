02d9b3b157798b2ca23192f3f4871aad3cca0a8f
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-08-30 11:28:06 +0530

    Time: 0 ms (100.00%) | Memory: 92.2 MB (22.14%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -3,25 +3,10 @@ public:
     int minimumDeletions(vector<int>& nums) {
         int n=nums.size();
 
-        int minIdx=-1,maxIdx=-1,mini=INT_MAX,maxi=INT_MIN;
+        int minIdx=min_element(nums.begin(),nums.end())-nums.begin();
+        int maxIdx=max_element(nums.begin(),nums.end())-nums.begin();
 
-        for(int i=0;i<n;i++){
-            if(nums[i]<mini){
-                mini=nums[i];
-                minIdx=i;
-            }
-        }
-
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]>maxi){
-                maxi=nums[i];
-                maxIdx=i;
-            }
-        }
-
-        bool flag=maxIdx<minIdx,flag1=maxIdx==minIdx;
-
-        if(flag) return min(maxIdx+1+n-minIdx,min(minIdx+1,n-maxIdx));
-        return min(minIdx+1+n-maxIdx,min(maxIdx+1,n-minIdx));
+        if(!(maxIdx<minIdx)) swap(minIdx,maxIdx);
+        return min(maxIdx+1+n-minIdx,min(minIdx+1,n-maxIdx));
     }
 };
