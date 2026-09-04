3a1c1c7198e91181c79c6ed82c739905e19a55b1
Piyush Raj <122628426+Piyusss@users.noreply.github.com>
2026-09-05 00:58:45 +0530

    Time: 0 ms (100.00%) | Memory: 31.7 MB (5.99%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& a,int k) {
+        int n=a.size();
+        vector<int>x(n,a[0]),y(n,a[n-1]);
+
+        for(int i=1;i<n;i++){
+            x[i]=max(x[i-1],a[i]);
+            y[n-1-i]=min(y[n-i],a[n-1-i]);
+        }
+
+        for(int i=0;i<n;i++) if(x[i]-y[i]<=k) return i;
+        return -1;
+    }
+};
